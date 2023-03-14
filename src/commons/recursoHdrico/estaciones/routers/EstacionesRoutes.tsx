d94b70751da6ddcf4fe19dfd311bb34b97a1da13
Alejandro Sastoque <sastoque42@gmail.com>
2023-03-14 12:03:48 -0500

    Routes

--- src/commons/recursoHdrico/estaciones/routers/EstacionesRoutes.tsx
+++ src/commons/recursoHdrico/estaciones/routers/EstacionesRoutes.tsx
@@ -1,13 +1,27 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
-import { IndexEstacionesScreen, GeolocalizacionScreen } from '../screens';
+import { IndexEstacionesScreen, GeolocalizacionScreen, AlertasScreen, AdministradorDeEstaciones, UsuariosScreen, HistorialScreen } from '../screens';
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
-export const RecursoHidricoRoutes: React.FC = () => {
+export const EstacionesRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<IndexEstacionesScreen />} />
-      <Route path="geolocalizacion" element={<GeolocalizacionScreen />} />
-      
+
+      {/* Partes Interesadas Estaciones */}
+      <Route path="geolocalizacion/*" element={<GeolocalizacionScreen />} />
+
+      {/* Configuracion Alerta persona */}
+      <Route path="alertas/*" element={<AlertasScreen />} />
+
+      {/* Estaciones */}
+      <Route path="estacion/*" element={<AdministradorDeEstaciones />} />
+
+      {/* Geolocalizacion Estaciones */}
+      <Route path="usuarios/*" element={<UsuariosScreen />} />
+
+      {/* Historial Estaciones */}
+      <Route path="historial/*" element={<HistorialScreen />} />
+
       <Route path="/*" element={<Navigate to={'/'} />} />
     </Routes>
   );
--- src/router/ProtectedRoutes.tsx
+++ src/router/ProtectedRoutes.tsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
 import { OrganigramaRoutes } from '../commons/gestorDocumental/organigrama/routes/OrganigramaRoutes';
 import { HomeRoutes } from '../commons/home/routes/HomeRoutes';
+import { EstacionesRoutes } from '../commons/recursoHdrico/estaciones/routers/EstacionesRoutes';
+import { RecursoHidricoRoutes } from '../commons/recursoHdrico/routers/RecursoHidricoRoutes';
 import { SeguridadRoutes } from '../commons/seguridad/routers/SeguridadRoutes';
 import { MainLayout } from '../layouts/MainLayout';
 
@@ -16,6 +18,10 @@ export const ProtectedRoutes: React.FC = () => {
         {/* Gestor documental */}
         <Route path="gestor_documental/*">
           <Route path="organigrama/*" element={<OrganigramaRoutes />} />
+        </Route>
+        {/* Recurso Hidrico */}
+        <Route path="recurso_hidrico/*" element={<RecursoHidricoRoutes />} >
+          <Route path="estaciones/*" element={<EstacionesRoutes />} />
         </Route>
       </Route>
     </Routes>
