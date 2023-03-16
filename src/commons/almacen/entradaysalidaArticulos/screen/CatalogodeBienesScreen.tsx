c5cab2230f3cdbf7029fd6a93fc9845f68c3331d
SneiderFuentes <67658428+SneiderFuentes@users.noreply.github.com>
2023-03-16 11:51:16 -0500

    Revert "Merge pull request #2 from macareniacorp/LorenaMartinez"
    This reverts commit 51480efd406d323a222d6113ee455cc0b3ef8fa4, reversing
    changes made to 8c0f401227baaddf1107cc96662147f73708a3ee.

--- src/commons/almacen/entradaysalidaArticulos/screen/CatalogodeBienesScreen.tsx
+++ src/commons/almacen/entradaysalidaArticulos/screen/CatalogodeBienesScreen.tsx
@@ -1,230 +1,27 @@
+import { Grid } from "@mui/material"
+import { Title } from '../../../../components/Title';
 
-import { useState, useEffect } from "react";
 
-
-import { useNavigate } from "react-router-dom";
-
-import { Column } from "primereact/column";
-import { TreeTable } from "primereact/treetable";
-import { InputText } from "primereact/inputtext";
-import AddIcon from "@mui/icons-material/Add";
-
-import {
-  obtener_todos_bienes,
-  seleccionar_bien_edit,
-  seleccionar_bien_create,
-  initial_state_bien,
-} from "../../../almacen/entradaysalidaArticulos/slices/indexCatalogodeBienes";
-import {
-  use_app_dispatch,
-  use_app_selector,
-} from "../../entradaysalidaArticulos/hooks/hooks";
-import "primeicons/primeicons.css";
-import { type INodo } from "../interfaces/Nodo"; 
-import "primereact/resources/themes/lara-light-indigo/theme.css";
-import { api } from "../../../../api/axios";
-import Button from "@mui/material/Button";
-
-// eslint-disable-next-line @typescript-eslint/naming-convention, @typescript-eslint/no-unused-vars
+// eslint-disable-next-line @typescript-eslint/naming-convention
 export const CatalogodeBienesScreen: React.FC = () => {
-  const bien = use_app_selector((state) => state.bien);
-  const dispatch = use_app_dispatch();
-
-  // const [buscarProducto, setBuscarProducto] = useState(true);
-  // const [bienesExampleH, setBienesExampleNodes] = useState<IBienes[]>([]);
-  const [global_filter1, set_global_filter1] = useState(null);
-  const [global_filter2, set_global_filter2] = useState(null);
-  const [array_total, set_array_total] = useState<INodo[]>([]);
-
-  useEffect(() => {
-    get_bienes();
-  }, []);
-
-  const get_bienes = async () => {
-    await api
-      .get("almacen/bienes/catalogo-bienes/get-list")
-      .then((bienes) => {
-        set_array_total(bienes.data.data);
-      })
-      .catch(() => {});
-  };
-  console.log(array_total, "estos son todos los datos");
-
-  // useEffect(() => {
-  //   armarArbol(bien);
-  // }, [bien]);
-
-  // function armarArbol(bienArray) {
-  //   let contador = 0;
-  //   bien.forEach((bienElement) => {
-  //     agregarNodosBase(bienElement, contador);
-  //     contador = array_total.length;
-  // });
-  // }
-
-  // const tree_table_func_map = {
-  //   global_filter1: set_global_filter1,
-  //   global_filter2: set_global_filter2,
-  // };
-
- 
-  // const get_header = (globalFilterKey: string) => {
-  //   return (
-  //     <div className="text-right">
-  //       <div className="p-input-icon-left">
-  //         <i className="pi pi-search"></i>
-  //         <InputText
-  //           type="search"
-  //           onInput={(e: any) =>
-  //             tree_table_func_map[`${globalFilterKey}`](e.target.value)
-  //           }
-  //           placeholder="Global Search"
-  //           size={50}
-  //         />
-  //       </div>
-  //     </div>
-  //   );
-  // };
-
-  // armarArbol();
-
-  const action_template = (
-    node: { data: { crear: any; editar: any; eliminar: any } },
-    Column: any
-  ) => {
     return (
-      <div>
-        <Button
-          type="button"
-          startIcon={<AddIcon />}
-          title="Agregar"
-          style={{ marginRight: ".5em", color: "black", border: "none" }}
-          onClick={() => {
-            enviar_datos(node, false); // crear
-          }}
-          disabled={!node.data.crear}
-        ></Button>
-        <Button
-          type="button"
-          startIcon={<AddIcon />}
-          className="p-button-white p-button-outlined"
-          title="Editar"
-          style={{ marginRight: ".5em", color: "black", border: "none" }}
-          onClick={() => {
-            enviar_datos(node, true); // true
-          }}
-          disabled={!node.data.editar}
-        ></Button>
-        <Button
-          type="button"
-          startIcon={<AddIcon />}
-          className="p-button-danger p-button-outlined"
-          title="Eliminar"
-          style={{ marginRight: ".5em", color: "black", border: "none" }}
-          disabled={!node.data.eliminar}
-          onClick={() => {
-            eliminarNodo(node); // true
-          }}
-        ></Button>
-      </div>
-    );
-  };
+      <>
+      <Title title='INFORMACIÓN GENERAL' />
+      <Grid
+        container
+        spacing={2}
+        sx={{
+          position: 'relative',
+          background: '#FAFAFA',
+          borderRadius: '15px',
+          p: '20px',
+          mb: '20px',
+          boxShadow: '0px 3px 6px #042F4A26',
+        }}
+      ></Grid></>
 
-  function enviar_datos(nodo: { data: any }, accion: boolean) : void {
-    if (accion) {
-      seleccionar_bien_edit(dispatch, nodo.data.bien);
-    } else {
-      seleccionar_bien_create(dispatch, nodo.data.bien);
-    }
-    navigate(
-      "/dashboard/Recaudo/gestor-notificacion/crear-entrada-articulos-fijos"
-    );
-  }
+        
+   
+    )
+}
 
-  function eliminarNodo(nodo: {
-    data: { crear: any; editar: any; eliminar: any } | { id_nodo: any };
-  }) : void {
-    // eliminarBien(dispatch, nodo);
-    void obtener_todos_bienes(dispatch);
-  }
-
-  const navigate = useNavigate();
-  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-  const crear_articulo = () => {
-    console.log("crear", initial_state_bien);
-    seleccionar_bien_create(dispatch, initial_state_bien.bienSeleccionado);
-    navigate(
-      "/dashboard/Recaudo/gestor-notificacion/crear-entrada-articulos-fijos"
-    );
-  };
-
-  return (
-    <div className="row min-vh-100">
-      <div className="col-lg-12 col-md-10 col-12 mx-auto">
-        <form
-          className="multisteps-form__panel border-radius-xl bg-white js-active p-4 position-relative"
-          data-animation="FadeIn"
-          id="configForm"
-        >
-          <div className="row">
-            <h3 className="text-rigth  fw-light mt-4 mb-2">
-              {" "}
-              Catálogo de bienes
-            </h3>
-          </div>
-
-          <div className="row">
-            <div className="col-12 col-md-6  mt-4">
-              <Button
-                variant="outlined"
-                startIcon={<AddIcon style={{ fontSize: "30px" }} />}
-                className=" mb-4"
-                type="button"
-                title="Agregar"
-                color="inherit"
-                style={{ fontSize: "20px", textTransform: "none" }}
-                onClick={() => { crear_articulo(); }}
-              >
-                Crear carpeta padre
-              </Button>
-            </div>
-          </div>
-          <div className="col-12 col-md-12  mt-4">
-            <div className="card">
-              <TreeTable value={array_total} filterMode="strict">
-                <Column
-                  expander
-                  body={<i className="fa-regular fa-folder fs-4"></i>}
-                  style={{ width: "250px" }}
-                ></Column>
-                <Column
-                  header="Nombre"
-                  field="nombre"
-                  style={{ width: "300px" }}
-                  filter
-                  filterPlaceholder="Filter por nombre"
-                ></Column>
-                <Column
-                  field="codigo"
-                  header="Código"
-                  style={{ width: "100px" }}
-                  filter
-                  filterPlaceholder="Filter por código"
-                ></Column>
-                <Column
-                  header="Acciones"
-                  body={action_template}
-                  style={{ textAlign: "center", width: "800px" }}
-                />
-                {/* <Column field="agregar" header="Agregar"></Column>
-                    <Column field="editar" header="Editar"></Column>
-                    <Column field="eliminar" header="Eliminar"></Column> */}
-              </TreeTable>
-              ;
-            </div>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
-};
