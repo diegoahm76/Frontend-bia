21e1ce3984b6bfb5fcaeeaf255a468bcaab469c8
Wilmer Novoa <wilmernovoa2002@gmail.com>
2023-03-17 18:56:26 -0500

    Pantalla Monitoreo Variables

--- src/commons/recursoHidrico/estaciones/screens/dashboardScreen.tsx
+++ src/commons/recursoHidrico/estaciones/screens/dashboardScreen.tsx
@@ -1,6 +1,416 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import { Chart as ChartJS, ArcElement, CategoryScale, LinearScale, PointElement, LineElement, Tooltip, Legend, Filler } from "chart.js";
+import { useEffect, useState } from "react";
+import { Grid, Stack, Typography, FormControl, } from '@mui/material';
+import { api } from "../../../../api/axios";
+import type { EstacionData } from "../interfaces/interfaces";
+import { Title } from '../../../../components/Title';
+import { Line } from "react-chartjs-2"
+import { Controller, useForm } from "react-hook-form";
+import Select from "react-select";
+import type { ChartOptions } from 'chart.js/auto';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import moment from 'moment';
+
+
+
+
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const DashboardScreen: React.FC = () => {
+
+    const [selectdashboards, set_select_dashboards] = useState({
+        opc_dashboards: 0,
+    });
+    const {
+        control,
+    } = useForm();
+
+    const opc_dashboards = [
+        { label: 'Estación Guamal', value: 1 },
+        { label: 'Estación Guayuriba', value: 2 },
+        { label: 'Estación Ocoa', value: 3 },
+        { label: 'Estación Puerto Gaitan', value: 4 },
+    ];
+    const [queryestaciones, setqueryestaciones] = useState<{
+        labels: any[];
+        datasets: any[];
+    }>({
+        labels: [],
+        datasets: [],
+    });
+    const [querytemperatura, setquerytemperatura] = useState<{
+        labels: any[];
+        datasets: any[];
+    }>({
+        labels: [],
+        datasets: [],
+    });
+    const [queryhumedad, setqueryhumedad] = useState<{
+        labels: any[];
+        datasets: any[];
+    }>({
+        labels: [],
+        datasets: [],
+    });
+    const [queryluminosidad, setqueryluminosidad] = useState<{
+        labels: any[];
+        datasets: any[];
+    }>({
+        labels: [],
+        datasets: [],
+    });
+    const [queryvelocidadagua, setqueryvelocidadagua] = useState<{
+        labels: any[];
+        datasets: any[];
+    }>({
+        labels: [],
+        datasets: [],
+    });
+    const [queryvelocidadviento, setqueryvelocidadviento] = useState<{
+        labels: any[];
+        datasets: any[];
+    }>({
+        labels: [],
+        datasets: [],
+    });
+    const [queryprecipitacion, setqueryprecipitacion] = useState<{
+        labels: any[];
+        datasets: any[];
+    }>({
+        labels: [],
+        datasets: [],
+    });
+    const [querydireccion, setquerydireccion] = useState<{
+        labels: any[];
+        datasets: any[];
+    }>({
+        labels: [],
+        datasets: [],
+    });
+    const [querynivelagua, setquerynivelagua] = useState<{
+        labels: any[];
+        datasets: any[];
+    }>({
+        labels: [],
+        datasets: [],
+    });
+
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    const [start_date, setstartdate] = useState<Date | null>(null);
+    const [end_date, setenddate] = useState<Date | null>(null);
+
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    const handle_start_date_change = (date: Date | null) => {
+        setstartdate(date);
+    };
+
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    const handle_end_date_change = (date: Date | null) => {
+        setenddate(date);
+    };
+    const get_datos_estaciones = async (): Promise<EstacionData[]> => {
+        // primera solicitud para filtrar por fechas
+        const { data: filtered_data } = await api.get(
+            `estaciones/datos/consultar-datos-fecha/${moment(start_date).format('YYYY-MM-DD')}/${moment(end_date).format('YYYY-MM-DD')}`
+        );
+        console.log(filtered_data);
+
+        const filtereddata = filtered_data.filter((datos: EstacionData) =>
+            datos.id_estacion === selectdashboards.opc_dashboards &&
+            moment(datos.fecha_registro).isBetween(moment(start_date), moment(end_date), null, '[]')
+        );
+
+        console.log(filtereddata);
+
+        const formatteddata = formatdataforchart(filtereddata);
+        setqueryestaciones(formatteddata);
+
+        const formatteddatatemperatura = formatdatafortemperatura(filtereddata);
+        setquerytemperatura(formatteddatatemperatura);
+
+        const formatteddatahumedad = formatdataforhumedad(filtereddata);
+        setqueryhumedad(formatteddatahumedad);
+
+        const formatteddataluminosidad = formatdataforluminosidad(filtereddata);
+        setqueryluminosidad(formatteddataluminosidad);
+
+        const formatteddatanivelagua = formatdatafornivelagua(filtereddata);
+        setquerynivelagua(formatteddatanivelagua);
+
+        const formatteddatavelocidadagua = formatdataforvelocidadagua(filtereddata);
+        setqueryvelocidadagua(formatteddatavelocidadagua);
+
+        const formatteddatavelocidaviento = formatdataforvelocidadviento(filtereddata);
+        setqueryvelocidadviento(formatteddatavelocidaviento);
+
+        const formatteddatadireccion = formatatafordireccion(filtereddata);
+        setquerydireccion(formatteddatadireccion);
+
+        const formatteddataprecipitacion = formatdataforprecipitacion(filtereddata);
+        setqueryprecipitacion(formatteddataprecipitacion);
+        return (filtered_data)
+    };
+    const options: ChartOptions = {
+        plugins: {
+            title: {
+                display: true,
+                text: ''
+            },
+            legend: {
+                position: 'top',
+            }
+        }
+    };
+
+    ChartJS.register(
+        ArcElement,
+        CategoryScale,
+        LinearScale,
+        PointElement,
+        LineElement,
+        Tooltip,
+        Legend,
+        Filler,
+    );
+
+    useEffect(() => {
+        void get_datos_estaciones();
+    }, [selectdashboards.opc_dashboards]);
+
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    const formatdataforchart = (data: EstacionData[]) => {
+        const labels = data.map((item) => item.fecha_registro);
+        const dataset = {
+            label: "Presion",
+            data: data.map((item) => item.presion_barometrica),
+            borderColor: "rgb(58, 158, 181)",
+            backgroundColor: "rgb(58, 158, 181)",
+        };
+        return { labels: labels, datasets: [dataset] };
+    };
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    const formatdatafortemperatura = (data: EstacionData[]) => {
+        const labels = data.map((item) => item.fecha_registro);
+        const dataset = {
+            label: "Temperatura",
+            data: data.map((item) => item.temperatura_ambiente),
+            borderColor: "rgb(58, 158, 181)",
+            backgroundColor: "rgb(58, 158, 181)",
+        };
+        return { labels: labels, datasets: [dataset] };
+    };
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    const formatdataforluminosidad = (data: EstacionData[]) => {
+        const labels = data.map((item) => item.fecha_registro);
+        const dataset = {
+            label: "Lux",
+            data: data.map((item) => item.luminosidad),
+            borderColor: "rgb(58, 158, 181)",
+            backgroundColor: "rgb(58, 158, 181)",
+        };
+        return { labels: labels, datasets: [dataset] };
+    };
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    const formatdatafornivelagua = (data: EstacionData[]) => {
+        const labels = data.map((item) => item.fecha_registro);
+        const dataset = {
+            label: "Nivel [m]",
+            data: data.map((item) => item.nivel_agua),
+            borderColor: "rgb(58, 158, 181)",
+            backgroundColor: "rgb(58, 158, 181)",
+        };
+        return { labels: labels, datasets: [dataset] };
+    };
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    const formatdataforvelocidadagua = (data: EstacionData[]) => {
+        const labels = data.map((item) => item.fecha_registro);
+        const dataset = {
+            label: "Velocidad m/s",
+            data: data.map((item) => item.velocidad_agua),
+            borderColor: "rgb(58, 158, 181)",
+            backgroundColor: "rgb(58, 158, 181)",
+        };
+        return { labels: labels, datasets: [dataset] };
+    };
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    const formatdataforvelocidadviento = (data: EstacionData[]) => {
+        const labels = data.map((item) => item.fecha_registro);
+        const dataset = {
+            label: "Velocidad viento",
+            data: data.map((item) => item.Velocidad_Viento),
+            borderColor: "rgb(58, 158, 181)",
+            backgroundColor: "rgb(58, 158, 181)",
+        };
+        return { labels: labels, datasets: [dataset] };
+    };
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    const formatdataforhumedad = (data: EstacionData[]) => {
+        const labels = data.map((item) => item.fecha_registro);
+        const dataset = {
+            label: "% Humedad",
+            data: data.map((item) => item.humedad_ambiente),
+            borderColor: "rgb(58, 158, 181)",
+            backgroundColor: "rgb(58, 158, 181)",
+        };
+        return { labels: labels, datasets: [dataset] };
+    };
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    const formatatafordireccion = (data: EstacionData[]) => {
+        const labels = data.map((item) => item.fecha_registro);
+        const dataset = {
+            label: "Direccion viento",
+            data: data.map((item) => item.direccion_viento),
+            borderColor: "rgb(58, 158, 181)",
+            backgroundColor: "rgb(58, 158, 181)",
+        };
+        return { labels: labels, datasets: [dataset] };
+    };
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    const formatdataforprecipitacion = (data: EstacionData[]) => {
+        const labels = data.map((item) => item.fecha_registro);
+        const dataset = {
+            label: "Precipitacion",
+            data: data.map((item) => item.precipitacion),
+            borderColor: "rgb(58, 158, 181)",
+            backgroundColor: "rgb(58, 158, 181)",
+        };
+        return { labels: labels, datasets: [dataset] };
+    };
+
     return (
-        <div><h1>Hola mundo</h1></div>
+        <>
+            <Grid
+                container
+                sx={{
+                    position: 'relative',
+                    background: '#FAFAFA',
+                    borderRadius: '15px',
+                    p: '20px',
+                    mb: '20px',
+                    boxShadow: '0px 3px 6px #042F4A26',
+                }}
+            >
+                <Grid item xs={12}>
+                    <Controller
+                        name="opcDashboard"
+                        control={control}
+                        render={({ field }) => (
+                            <FormControl fullWidth>
+                                <Title title="Comportamiento de las variables" />
+                                <Stack direction="row" spacing={2} sx={{ m: '20px 0' }}>
+                                    <Controller
+                                        name="opcDashboard"
+                                        control={control}
+                                        render={({ field }) => (
+                                            <Select
+                                                {...field}
+                                                onChange={(e) => {
+                                                    set_select_dashboards({
+                                                        ...selectdashboards,
+                                                        opc_dashboards: e.value,
+                                                    });
+                                                }
+                                                }
+                                                options={opc_dashboards}
+                                                placeholder="Seleccionar"
+                                            />
+                                        )}
+                                    />
+                                </Stack>
+                            </FormControl>
+                        )}
+                    />
+                    <Typography variant="body1" hidden={selectdashboards.opc_dashboards !== 1}>
+
+                        <DatePicker
+                            selected={start_date}
+                            onChange={handle_start_date_change}
+                            placeholderText="Fecha inicial"
+                        />
+                        <DatePicker
+                            selected={end_date}
+                            onChange={handle_end_date_change}
+                            placeholderText="Fecha Final"
+                        />
+
+                        <Line data={queryestaciones} options={options} />
+                        <Line data={queryhumedad} options={options} />
+                        <Line data={querydireccion} options={options} />
+                        <Line data={queryprecipitacion} options={options} />
+                        <Line data={queryluminosidad} options={options} />
+                        <Line data={querytemperatura} options={options} />
+                        <Line data={queryvelocidadviento} options={options} />
+                        <Line data={queryvelocidadagua} options={options} />
+                        <Line data={querynivelagua} options={options} />
+                    </Typography>
+                    <Typography variant="body1" hidden={selectdashboards.opc_dashboards !== 2}>
+                        <DatePicker
+                            selected={start_date}
+                            onChange={handle_start_date_change}
+                            placeholderText="Fecha inicial"
+                        />
+                        <DatePicker
+                            selected={end_date}
+                            onChange={handle_end_date_change}
+                            placeholderText="Fecha Final"
+                        />
+                        <Line data={queryestaciones} options={options} />
+                        <Line data={queryhumedad} options={options} />
+                        <Line data={querydireccion} options={options} />
+                        <Line data={queryprecipitacion} options={options} />
+                        <Line data={queryluminosidad} options={options} />
+                        <Line data={querytemperatura} options={options} />
+                        <Line data={queryvelocidadviento} options={options} />
+                        <Line data={queryvelocidadagua} options={options} />
+                        <Line data={querynivelagua} options={options} />
+                    </Typography>
+                    <Typography variant="body1" hidden={selectdashboards.opc_dashboards !== 3}>
+                        <DatePicker
+                            selected={start_date}
+                            onChange={handle_start_date_change}
+                            placeholderText="Fecha inicial"
+                        />
+                        <DatePicker
+                            selected={end_date}
+                            onChange={handle_end_date_change}
+                            placeholderText="Fecha Final"
+                        />
+                        <Line data={queryestaciones} options={options} />
+                        <Line data={queryhumedad} options={options} />
+                        <Line data={querydireccion} options={options} />
+                        <Line data={queryprecipitacion} options={options} />
+                        <Line data={queryluminosidad} options={options} />
+                        <Line data={querytemperatura} options={options} />
+                        <Line data={queryvelocidadviento} options={options} />
+                        <Line data={queryvelocidadagua} options={options} />
+                        <Line data={querynivelagua} options={options} />
+                    </Typography>
+                    <Typography variant="body1" hidden={selectdashboards.opc_dashboards !== 4}>
+                        <DatePicker
+                            selected={start_date}
+                            onChange={handle_start_date_change}
+                            placeholderText="Fecha inicial"
+                        />
+                        <DatePicker
+                            selected={end_date}
+                            onChange={handle_end_date_change}
+                            placeholderText="Fecha Final"
+                        />
+                        <Line data={queryestaciones} options={options} />
+                        <Line data={queryhumedad} options={options} />
+                        <Line data={querydireccion} options={options} />
+                        <Line data={queryprecipitacion} options={options} />
+                        <Line data={queryluminosidad} options={options} />
+                        <Line data={querytemperatura} options={options} />
+                        <Line data={queryvelocidadviento} options={options} />
+                        <Line data={queryvelocidadagua} options={options} />
+                        <Line data={querynivelagua} options={options} />
+                    </Typography>
+                </Grid>
+            </Grid>
+        </>
+
     )
 }
+
