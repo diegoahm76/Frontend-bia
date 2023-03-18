645280c60671cb05da908ca10758e221b71ea7ef
Elver Rodriguez <adelson9602@gmail.com>
2023-03-18 04:33:03 -0500

    Se implementa formulario nuevo con validaciones refactorizadas

--- src/interfaces/globalModels.ts
+++ src/interfaces/globalModels.ts
@@ -1,11 +1,35 @@
-export interface ResponseThunks<T = any | null>{
-  ok: boolean,
-  data?: T,
-  error_message?: string 
+export interface ResponseThunks<T = any | null> {
+  ok: boolean;
+  data?: T;
+  error_message?: string;
 }
 
 export interface ResponseServer<T> {
   success: boolean;
   detail: string;
   data: T;
+}
+
+export interface Paises {
+  nombre: string;
+  cod_pais: string;
+}
+
+export interface TipoDocumento {
+  cod_tipo_documento: string;
+  nombre: string;
+}
+
+export interface TipoPersona {
+  cod_tipo_persona: string;
+  tipo_persona: string;
+}
+
+export interface IGeneric {
+  label: string;
+  value: string;
+}
+export interface IList {
+  label: string;
+  value: string;
 }
