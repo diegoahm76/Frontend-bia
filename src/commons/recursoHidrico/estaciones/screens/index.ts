78199a08cbd3baf06e1f7854870df84121e1c411
GuillermoSarmiento <guillermo.sarmiento@macarenia.org>
2023-03-22 18:23:54 -0500

    Cambio Nombres

--- src/commons/recursoHidrico/estaciones/screens/index.ts
+++ src/commons/recursoHidrico/estaciones/screens/index.ts
@@ -1,6 +1,4 @@
 export * from './IndexEstacionesScreen';
-export * from './GeolocalizacionScreen';
 export * from './UsuariosScreen';
 export * from './AlertasScreen';
 export * from './AdministradorDeEstaciones';
-export * from './dashboardScreen'
