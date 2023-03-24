590b290b7520d56dd4f7cc0f9533991b9181e7ff
unknown <oscars.santamaria@gmail.com>
2023-03-24 13:44:58 -0500

    se estructura rutas en almacen en cuantoa carpetas y pantallaz dejandolas listas para migrar y crear codigo

---
+++ src/commons/almacen/registroSolicitudesAlmacen/screens/SolicitudBienesConsumoViverosScreen.tsx
@@ -0,0 +1,13 @@
+
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export function SolicitudBienesConsumoViverosScreen(): JSX.Element {
+ 
+
+    return (
+      <>
+      <h1>Solicitud benes Consumo viveros</h1>
+      </>
+    );
+  }
+  
--- src/commons/almacen/routes/AlmacenRoutes.tsx
+++ src/commons/almacen/routes/AlmacenRoutes.tsx
@@ -3,6 +3,11 @@ import { Page404 } from '../../../screens/404';
 import { ConfiguracionRoutes } from '../configuracion/routes/ConfiguracionRoutes';
 import { EntradaysalidArticulosRoutes } from '../entradaysalidaArticulos/routes/EntradaysalidaArticulosRoutes';
 import { GestionDeInventarioRoutes } from '../gestionDeInventario/routes/GestionDeInventarioRoutes';
+import { EntradaBienesAlmacenRoutes } from '../entradaDeAlmacen/routes/EntradaAlmacenRoutes';
+import { RegistroSolicitudesAlmacenRoutes } from '../registroSolicitudesAlmacen/routes/RegistroSolicitudesAlmacenRoutes';
+import { ReportesIndicadoresAnaliticaRoutes } from '../reportesIndicadoresAnalitca/routes/ReportesIndicadoresAnaliticaRoutes';
+import { ConfiguracionDatosBasicosRoutes } from '../configuracionDatosBasicos/routes/ConfiguracionDatosBasicosRoutes';
+
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const AlmacenRoutes: React.FC = () => {
@@ -16,6 +21,22 @@ export const AlmacenRoutes: React.FC = () => {
       <Route
         path="gestion_inventario/*"
         element={<GestionDeInventarioRoutes />}
+      />
+      <Route
+        path="entrada_almacen/*"
+        element={<EntradaBienesAlmacenRoutes />}
+      />
+      <Route
+        path="registro_solicitudes_almacen/*"
+        element={<RegistroSolicitudesAlmacenRoutes />}
+      />
+      <Route
+        path="reportes_indicadores_analitica/*"
+        element={<ReportesIndicadoresAnaliticaRoutes />}
+      />
+      <Route
+        path="configuracion_datos_basicos/*"
+        element={<ConfiguracionDatosBasicosRoutes />}
       />
       <Route path="/*" element={<Page404 />} />
     </Routes>
