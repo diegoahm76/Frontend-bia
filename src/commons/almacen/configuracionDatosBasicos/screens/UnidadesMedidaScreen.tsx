5a65fe9451ee04150a26338905b3b97c53395531
GuillermoSarmiento <guillermo.sarmiento@macarenia.org>
2023-03-27 21:07:56 -0500

    Restaurar merge

---
+++ src/commons/almacen/configuracionDatosBasicos/screens/UnidadesMedidaScreen.tsx
@@ -0,0 +1,13 @@
+
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export function UnidadesMedidaScreen(): JSX.Element {
+ 
+
+    return (
+      <>
+      <h1>Unidades medida</h1>
+      </>
+    );
+  }
+  
---
+++ src/commons/almacen/entradaDeAlmacen/screens/EntradaBienesAlmacenScreen.tsx
@@ -0,0 +1,13 @@
+
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export function EntradaBienesAlmacenScreen(): JSX.Element {
+ 
+
+    return (
+      <>
+      <h1>Entrada de bienes de Almacen</h1>
+      </>
+    );
+  }
+  
