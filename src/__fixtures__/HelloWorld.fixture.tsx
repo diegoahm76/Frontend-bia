7647d8460970ed4a54da76ddea18b086694dbf54
GuillermoSarmiento <guillermo.sarmiento@macarenia.org>
2023-04-03 10:40:03 -0500

    Prueba React cosmos

--- src/__fixtures__/HelloWorld.fixture.tsx
+++ src/__fixtures__/HelloWorld.fixture.tsx
@@ -1,14 +1,15 @@
-import { Grid } from "@mui/material";
-import { HelloWorld } from "./components/HelloWorld"
+import React from "react"
+// import { Grid } from "@mui/material";
+// import { HelloWorld } from "./components/HelloWorld"
 
-export default function Hello(){
-    return(
-        <Grid>
-            <HelloWorld text='HelloWorld'/>
-        </Grid>
-    );
-}
+// export default function Hello(){
+//     return(
+//         <Grid>
+//             <HelloWorld text='HelloWorld'/>
+//         </Grid>
+//     );
+// }
 
-// export default{
-//     primary: <h1>primary</h1>
-//   }
+export default{
+    primary: <h1>primary</h1>
+  }
