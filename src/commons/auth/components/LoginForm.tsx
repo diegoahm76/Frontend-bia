32eafe1a710d7655f5e00bd02eb47f354fb9d519
GuillermoSarmiento <guillermo.sarmiento@macarenia.org>
2023-04-13 15:46:54 -0500

    Correcion de tildes

--- src/commons/auth/components/LoginForm.tsx
+++ src/commons/auth/components/LoginForm.tsx
@@ -238,7 +238,7 @@ export const LoginForm: React.FC = () => {
           >
             <Typography sx={{ fontSize: '17px' }}>Bienvenido</Typography>
             <Typography sx={{ fontSize: '13px', textAlign: 'justify' }}>
-              Este es nuestro portal de trámites en lineá un sistema fácil de
+              Este es nuestro portal de trámites en linea un sistema fácil de
               usar, ágil y sencillo para el control general de solicitudes y
               requerimientos ambientales en el departamento del Meta.
             </Typography>
