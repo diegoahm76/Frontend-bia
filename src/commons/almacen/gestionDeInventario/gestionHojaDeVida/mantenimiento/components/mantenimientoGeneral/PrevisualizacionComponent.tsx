e98b79056f7bac17fb045ae7fae4f32401b02971
Pablo Leonardo Barrera Rodriguez <98960094+Palebaro@users.noreply.github.com>
2023-04-20 13:45:22 -0500

    reemplazo forech por for

--- src/commons/almacen/gestionDeInventario/gestionHojaDeVida/mantenimiento/components/mantenimientoGeneral/PrevisualizacionComponent.tsx
+++ src/commons/almacen/gestionDeInventario/gestionHojaDeVida/mantenimiento/components/mantenimientoGeneral/PrevisualizacionComponent.tsx
@@ -11,9 +11,9 @@ interface IProps {
 export const PrevisualizacionComponent:React.FC<IProps> = ({data_grid}:IProps) => {
     const [data_previsualizacion, set_data_previsualizacion] = useState<crear_mantenimiennto[]>([]);
     useEffect(() =>{
-        data_grid.forEach(nm =>{
-            set_data_previsualizacion(prevArray => [...prevArray, nm])
-        });
+        for(let m = 0; m < data_grid.length; m++ ){
+            set_data_previsualizacion(prevArray => [...prevArray, data_grid[m]])
+        }
     },[data_grid]);
 
     const columns = [
