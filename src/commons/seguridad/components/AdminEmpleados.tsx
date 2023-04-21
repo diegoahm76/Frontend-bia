c90a812db4c180da65a3bd441d28bd388cb4a631
Wilmer Novoa <wilmernovoa2002@gmail.com>
2023-04-21 16:02:58 -0500

    Admin Empleados

---
+++ src/commons/seguridad/components/AdminEmpleados.tsx
@@ -0,0 +1,16 @@
+import { Grid } from "@mui/material";
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export function AdminEmpleados(): JSX.Element {
+
+
+  return (
+    <>
+
+      <Grid item>
+        <h1>Hola mundo</h1>
+      </Grid>
+
+    </>
+  );
+}
