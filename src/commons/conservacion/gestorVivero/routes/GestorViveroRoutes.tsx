2b4271b902ad77366df948c8bead1ccc20e2db4d
DevMacarenia <redes@macarenia.org>
2023-04-27 10:16:54 -0500

    Asignar ruta responsable viveros

--- src/commons/conservacion/gestorVivero/routes/GestorViveroRoutes.tsx
+++ src/commons/conservacion/gestorVivero/routes/GestorViveroRoutes.tsx
@@ -5,6 +5,7 @@ import { BajaHerramientaScreen,  } from '../screens/BajaHerramientaScreen';
 import { CuarentenaViveroScreen,  } from '../screens/CuarentenaViveroScreen';
 import { DespachoViveroScreen,  } from '../screens/DespachoViveroScreen';
 import { AsignarResponsableViveroScreen  } from '../screens/AsignarResponsableVivero';
+import { Page404 } from '../../../../screens/404';
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const GestorViveroRoutes: React.FC = () => {
   return (
@@ -15,8 +16,8 @@ export const GestorViveroRoutes: React.FC = () => {
       <Route path="administrar_viveros" element={<AdministrarViveroScreen/>} />
       <Route path="bajas" element={<BajaHerramientaScreen/>} />
       <Route path="recepcion_distribucion" element={<DespachoViveroScreen/>} />
-      <Route path="responsable_viveros/:id?" element={<AsignarResponsableViveroScreen/>} />
-      <Route path="/*" element={<Navigate to={'/'} />} />
+      <Route path="viverista/:id?" element={<AsignarResponsableViveroScreen/>} />
+      <Route path="/*" element={<Page404 />} />
     </Routes>
   );
 };
