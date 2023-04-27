5374cd8c96c0d653f5360027f6300f2394839703
DevMacarenia <redes@macarenia.org>
2023-04-27 10:07:40 -0500

    Asignar ruta responsable viveros

--- src/commons/conservacion/gestorVivero/routes/GestorViveroRoutes.tsx
+++ src/commons/conservacion/gestorVivero/routes/GestorViveroRoutes.tsx
@@ -5,18 +5,17 @@ import { BajaHerramientaScreen,  } from '../screens/BajaHerramientaScreen';
 import { CuarentenaViveroScreen,  } from '../screens/CuarentenaViveroScreen';
 import { DespachoViveroScreen,  } from '../screens/DespachoViveroScreen';
 import { AsignarResponsableViveroScreen  } from '../screens/AsignarResponsableVivero';
-
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const GestorViveroRoutes: React.FC = () => {
   return (
     
-    <Routes>
-      <Route path="administrar_viveros" element={<AdministrarViveroScreen/>} />
+    <Routes>      
       <Route path="apertura_cierre/:id?" element={<AperturaCierreViveroScreen/>} />
-      <Route path="bajas" element={<BajaHerramientaScreen/>} />
       <Route path="cuarentena/:id?" element={<CuarentenaViveroScreen/>} />
+      <Route path="administrar_viveros" element={<AdministrarViveroScreen/>} />
+      <Route path="bajas" element={<BajaHerramientaScreen/>} />
       <Route path="recepcion_distribucion" element={<DespachoViveroScreen/>} />
-      <Route path="viverista/:id?" element={<AsignarResponsableViveroScreen/>} />
+      <Route path="responsable_viveros/:id?" element={<AsignarResponsableViveroScreen/>} />
       <Route path="/*" element={<Navigate to={'/'} />} />
     </Routes>
   );
