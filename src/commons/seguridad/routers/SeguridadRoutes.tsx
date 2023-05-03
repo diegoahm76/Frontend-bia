ff2ebe05867c5a5e550a569aa7cc3aa9c1b90b54
amadoxxo <elian12478amadiss@gmail.com>
2023-05-03 18:27:27 -0500

    Avance seguridad pantalla Mis Datos acceso

--- src/commons/seguridad/routers/SeguridadRoutes.tsx
+++ src/commons/seguridad/routers/SeguridadRoutes.tsx
@@ -9,6 +9,7 @@ import { Page404 } from '../../../screens/404';
 import AuditoriaScreen from '../screens/AuditoriaScreen';
 import { DelegacionSuperuserScreen } from '../screens/DelegacionSuperuserScreen';
 import { DatosPersonalesScreen } from '../screens/DatosPersonalesScreen';
+import { DatosAccesoScreen } from '../screens/DatosAccesoScreen';
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const SeguridadRoutes: React.FC = () => {
@@ -23,7 +24,9 @@ export const SeguridadRoutes: React.FC = () => {
         element={<AdministracionPersonas />}
       />
       <Route path="/delegacion_superusuario" element={<DelegacionSuperuserScreen />}/>
-      <Route path="/datos_personales" element={<DatosPersonalesScreen />}></Route>
+      <Route path="/datos_personales" element={<DatosPersonalesScreen />} />
+      <Route path="/datos_acceso" element={<DatosAccesoScreen />} />
+
       <Route path="/*" element={<Page404 />} />
     </Routes>
   );
