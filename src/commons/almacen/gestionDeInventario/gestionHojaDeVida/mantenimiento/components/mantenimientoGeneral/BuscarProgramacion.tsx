13ad03bca1bff1dd9f3116e56a0170046d88e7c1
amadoxxo <elian12478amadiss@gmail.com>
2023-05-04 18:37:37 -0500

    avance seguridad Modal pantalla Datos Acceso

--- src/commons/almacen/gestionDeInventario/gestionHojaDeVida/mantenimiento/components/mantenimientoGeneral/BuscarProgramacion.tsx
+++ src/commons/almacen/gestionDeInventario/gestionHojaDeVida/mantenimiento/components/mantenimientoGeneral/BuscarProgramacion.tsx
@@ -1,15 +1,25 @@
-import { Box, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Grid, Stack, TextField } from "@mui/material"
-import { useEffect, useState, type Dispatch, type SetStateAction } from "react";
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Grid,
+  Stack,
+  TextField,
+} from '@mui/material';
+import { useEffect, useState, type Dispatch, type SetStateAction } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
-import { Column } from "primereact/column";
-import { DataTable } from "primereact/datatable";
+import { Column } from 'primereact/column';
+import { DataTable } from 'primereact/datatable';
 import { Title } from '../../../../../../../components';
-import { useAppDispatch } from "../../../../../../../hooks";
-import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import dayjs from "dayjs";
-import { get_programmed_maintenance } from "./thunks/maintenanceThunks";
-
+import { useAppDispatch } from '../../../../../../../hooks';
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs from 'dayjs';
+import { get_programmed_maintenance } from './thunks/maintenanceThunks';
 
 interface IProps {
   is_modal_active: boolean;
@@ -22,10 +32,10 @@ const BuscarProrgamacionComponent = ({
   is_modal_active,
   set_is_modal_active,
   title,
-  parent_details
+  parent_details,
 }: IProps) => {
   const dispatch = useAppDispatch();
-  const [codigo, set_codigo] = useState<string>("");
+  const [codigo, set_codigo] = useState<string>('');
   const [grid_busqueda, set_grid_busqueda] = useState<any[]>([]);
   const [grid_busqueda_before, set_grid_busqueda_before] = useState<any[]>([]);
   const [selected_product, set_selected_product] = useState(null);
@@ -34,7 +44,7 @@ const BuscarProrgamacionComponent = ({
 
   const on_change_codigo: any = (e: React.ChangeEvent<HTMLInputElement>) => {
     set_codigo(e.target.value);
-  }
+  };
 
   const handle_change_fecha_desde = (date: Date | null): void => {
     set_fecha_desde(date);
@@ -45,16 +55,21 @@ const BuscarProrgamacionComponent = ({
   };
 
   const accionar_busqueda: any = () => {
-    dispatch(get_programmed_maintenance(dayjs(fecha_desde).format("DD-MM-YYYY"), dayjs(fecha_hasta).format("DD-MM-YYYY"))).then((response: any) => {
+    dispatch(
+      get_programmed_maintenance(
+        dayjs(fecha_desde).format('DD-MM-YYYY'),
+        dayjs(fecha_hasta).format('DD-MM-YYYY')
+      )
+    ).then((response: any) => {
       set_grid_busqueda(response.detail);
       set_grid_busqueda_before([...response.detail]);
       console.log(grid_busqueda_before);
-    })
-  }
+    });
+  };
 
   const selected_product_grid: any = () => {
     set_is_modal_active(false);
-  }
+  };
 
   useEffect(() => {
     parent_details(selected_product);
@@ -66,11 +81,16 @@ const BuscarProrgamacionComponent = ({
       fullWidth
       maxWidth="md"
       open={is_modal_active}
-      onClose={() => { set_is_modal_active(false); }}
+      onClose={() => {
+        set_is_modal_active(false);
+      }}
     >
       <DialogTitle>{title}</DialogTitle>
       <DialogContent>
-        <DialogContentText id="alert-dialog-slide-description">
+        <DialogContentText
+          component={'span'}
+          id="alert-dialog-slide-description"
+        >
           <Box
             component="form"
             sx={{ mt: '20px' }}
@@ -88,37 +108,31 @@ const BuscarProrgamacionComponent = ({
                   onChange={on_change_codigo}
                 />
               </Grid>
-              <Grid item xs={12} sm={3} >
+              <Grid item xs={12} sm={3}>
                 <LocalizationProvider dateAdapter={AdapterDayjs}>
                   <DatePicker
                     label="Fecha desde"
                     value={fecha_desde}
-                    onChange={(newValue) => { handle_change_fecha_desde(newValue); }}
+                    onChange={(newValue) => {
+                      handle_change_fecha_desde(newValue);
+                    }}
                     renderInput={(params) => (
-                      <TextField
-                        required
-                        fullWidth
-                        size="small"
-                        {...params}
-                      />
+                      <TextField required fullWidth size="small" {...params} />
                     )}
                     maxDate={fecha_hasta}
                   />
                 </LocalizationProvider>
               </Grid>
-              <Grid item xs={12} sm={3} >
+              <Grid item xs={12} sm={3}>
                 <LocalizationProvider dateAdapter={AdapterDayjs}>
                   <DatePicker
                     label="Fecha hasta"
                     value={fecha_hasta}
-                    onChange={(newValue) => { handle_change_fecha_hasta(newValue); }}
+                    onChange={(newValue) => {
+                      handle_change_fecha_hasta(newValue);
+                    }}
                     renderInput={(params) => (
-                      <TextField
-                        required
-                        fullWidth
-                        size="small"
-                        {...params}
-                      />
+                      <TextField required fullWidth size="small" {...params} />
                     )}
                     minDate={fecha_desde}
                     disabled={fecha_desde == null}
@@ -132,8 +146,8 @@ const BuscarProrgamacionComponent = ({
               >
                 <Grid item xs={12} sm={3}>
                   <Button
-                    color='primary'
-                    variant='contained'
+                    color="primary"
+                    variant="contained"
                     startIcon={<SearchIcon />}
                     onClick={accionar_busqueda}
                   >
@@ -141,9 +155,9 @@ const BuscarProrgamacionComponent = ({
                   </Button>
                 </Grid>
               </Stack>
-
             </Grid>
-            <Grid container
+            <Grid
+              container
               sx={{
                 position: 'relative',
                 background: '#FAFAFA',
@@ -151,17 +165,42 @@ const BuscarProrgamacionComponent = ({
                 p: '20px',
                 mb: '20px',
                 boxShadow: '0px 3px 6px #042F4A26',
-              }}>
+              }}
+            >
               <Grid item xs={12} sm={12}>
-                <Title title='Resultados' />
+                <Title title="Resultados" />
                 <Box sx={{ width: '100%', mt: '20px' }}>
                   <div className="card">
-                    <DataTable value={grid_busqueda} sortField="nombre" stripedRows paginator rows={5} rowsPerPageOptions={[5, 10, 25, 50]} tableStyle={{ minWidth: '50rem' }}
-                      selectionMode="single" selection={selected_product} onSelectionChange={(e) => { set_selected_product(e.value); }} dataKey="id_programacion_mantenimiento"
+                    <DataTable
+                      value={grid_busqueda}
+                      sortField="nombre"
+                      stripedRows
+                      paginator
+                      rows={5}
+                      rowsPerPageOptions={[5, 10, 25, 50]}
+                      tableStyle={{ minWidth: '50rem' }}
+                      selectionMode="single"
+                      selection={selected_product}
+                      onSelectionChange={(e) => {
+                        set_selected_product(e.value);
+                      }}
+                      dataKey="id_programacion_mantenimiento"
                     >
-                      <Column field="articulo" header="Artículo" style={{ width: '20%' }}></Column>
-                      <Column field="tipo_descripcion" header="Tipo mantenimiento" style={{ width: '40%' }}></Column>
-                      <Column field="fecha" header="Fecha programado" style={{ width: '40%' }}></Column>
+                      <Column
+                        field="articulo"
+                        header="Artículo"
+                        style={{ width: '20%' }}
+                      ></Column>
+                      <Column
+                        field="tipo_descripcion"
+                        header="Tipo mantenimiento"
+                        style={{ width: '40%' }}
+                      ></Column>
+                      <Column
+                        field="fecha"
+                        header="Fecha programado"
+                        style={{ width: '40%' }}
+                      ></Column>
                     </DataTable>
                   </div>
                 </Box>
@@ -172,16 +211,24 @@ const BuscarProrgamacionComponent = ({
       </DialogContent>
       <DialogActions>
         <Button
-          color='primary'
-          variant='contained'
-          onClick={selected_product_grid}>Seleccionar</Button>
+          color="primary"
+          variant="contained"
+          onClick={selected_product_grid}
+        >
+          Seleccionar
+        </Button>
         <Button
-          color='inherit'
-          variant='contained'
-          onClick={() => { set_is_modal_active(false); }}>Salir</Button>
+          color="inherit"
+          variant="contained"
+          onClick={() => {
+            set_is_modal_active(false);
+          }}
+        >
+          Salir
+        </Button>
       </DialogActions>
     </Dialog>
-  )
-}
+  );
+};
 // eslint-disable-next-line no-restricted-syntax
 export default BuscarProrgamacionComponent;
