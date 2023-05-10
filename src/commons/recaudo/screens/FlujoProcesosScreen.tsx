b63f4dc27c3d6d6dc89627cd774111ae598bfcc9
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-05-10 11:45:53 -0500

    Agregué estados y moví los inputs a un nuevo modal, además agregué los inputs restantes para poder crear un nuevo flujo.

--- src/commons/recaudo/screens/FlujoProcesosScreen.tsx
+++ src/commons/recaudo/screens/FlujoProcesosScreen.tsx
@@ -1,18 +1,17 @@
 import {
   Box,
-  FormControl,
   Grid,
-  InputLabel,
-  MenuItem,
-  Select,
   Stack,
   type SelectChangeEvent,
   Button
 } from "@mui/material"
 import { Title } from "../../../components"
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { FlowChart } from '../components/flowChart/FlowChart';
 import axios from "axios";
+import { getLayoutedElements } from "../components/flowChart/LayoutedElements/getLayoutedElements";
+import { AddFlujoProcesoModal } from "../components/flujoProcesos/modal/AddFlujoProcesoModal";
+import type { Etapa } from "../interfaces/proceso";
 
 interface Nodes {
   id: number;
@@ -34,38 +33,109 @@ interface Dataflow {
   edges: Edges[];
 }
 
-const tipo_desde = [
-  { value: 'cobro_coactivo', label: 'Cobro Coactivo' },
-  { value: 'embargo', label: 'Embargo' },
-  { value: 'mandamiento_pago', label: 'Mandamiento de Pago' },
-  { value: 'remate', label: 'Remate' },
-  { value: 'cobro_persuasivo', label: 'Cobro persuasivo' },
-]
+interface FormDataFlujo {
+  id_etapa_origen: string;
+  id_etapa_destino: string;
+  fecha_flujo: string;
+  descripcion: string;
+  requisitos: string;
+}
+
+// const tipo_desde = [
+//   { value: 'cobro_coactivo', label: 'Cobro Coactivo' },
+//   { value: 'embargo', label: 'Embargo' },
+//   { value: 'mandamiento_pago', label: 'Mandamiento de Pago' },
+//   { value: 'remate', label: 'Remate' },
+//   { value: 'cobro_persuasivo', label: 'Cobro persuasivo' },
+// ]
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const FlujoProcesosScreen: React.FC = () => {
   const [dataflow, set_dataflow] = useState<Dataflow>();
-  const [tipo, set_tipo] = useState("");
-  const [tipo_hasta, set_tipo_hasta] = useState("");
-
+  const [etapas_proceso, set_etapas_proceso] = useState<Etapa[]>([]);
+  const [form_data_flujo, set_form_data_flujo] = useState<FormDataFlujo>({
+    id_etapa_origen: '',
+    id_etapa_destino: '',
+    fecha_flujo: '',
+    descripcion: '',
+    requisitos: '',
+  });
+  const [add_flujo_proceso, set_add_flujo_proceso] = useState<boolean>(false);
+  const [is_submit, set_is_submit] = useState<boolean>(false);
+  
   useEffect(() => {
-    axios.get('/db/dataflow.json')
+    axios.get('http://macarenia.bitpointer.co/api/recaudo/procesos/grafica')
       .then((response) => {
         set_dataflow(response.data);
       })
       .catch((error) => {
         console.log(error);
       });
+  }, [is_submit]);
+
+  useEffect(() => {
+    axios.get('http://macarenia.bitpointer.co/api/recaudo/procesos/etapas/')
+      .then((response) => {
+        set_etapas_proceso(response.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
+  
+  // const { nodes, edges } = getLayoutedElements(dataflow?.nodes, dataflow?.edges);
+  const nodes_edges = useCallback(() => {
+    const new_nodes = dataflow?.nodes.map((node) => ({
+      id: node.id.toString(),
+      data: node.data,
+      type: 'infoFlujoNode',
+      position: { x: 0, y: 0 },
+    }));
 
+    const new_edges = dataflow?.edges.map((edge) => ({
+      id: edge.id.toString(),
+      source: edge.source.toString(),
+      target: edge.target.toString(),
+      type: 'smoothstep',
+      style: {
+        stroke: 'black',
+        strokeWidth: 2
+      },
+      markerEnd: {
+        type: 'arrowclosed',
+        color: 'black',
+      }
+    }));
 
-  const handle_change: (event: SelectChangeEvent) => void = (event: SelectChangeEvent) => {
-    set_tipo(event.target.value);
-  }
+    return getLayoutedElements(new_nodes, new_edges);
+  }, [dataflow]);
 
-  const handle_change_hasta: (event: SelectChangeEvent) => void = (event: SelectChangeEvent) => {
-    set_tipo_hasta(event.target.value);
-  }
+  const handle_input_change = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = event.target;
+    set_form_data_flujo(prevFormData => ({ ...prevFormData, [name]: value }));
+  };
+
+  const handle_select_change: (event: SelectChangeEvent) => void = (event: SelectChangeEvent) => {
+    const { name, value } = event.target;
+    set_form_data_flujo(prevFormData => ({ ...prevFormData, [name]: value }));
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const handle_submit = (): void => {
+    axios.post('http://macarenia.bitpointer.co/api/recaudo/procesos/flujos/', {
+      ...form_data_flujo
+    })
+      .then((response) => {
+        console.log(response);
+        set_is_submit(true);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        set_is_submit(false);
+      });
+  };
 
   return (
     <>
@@ -81,7 +151,7 @@ export const FlujoProcesosScreen: React.FC = () => {
         }}
       >
         <Grid item xs={12}>
-          <Title title="Flujo Proceso"></Title>
+          <Title title="Flujo de Procesos"></Title>
           <Box
             component='form'
             sx={{ mt: '20px' }}
@@ -98,57 +168,35 @@ export const FlujoProcesosScreen: React.FC = () => {
               }}
             >
               {/* AQUI ES DONDE ESTARIA EL FLUJO DE PROCESO */}
-              <FlowChart dataflow={dataflow} />
+              <FlowChart initialNodes={nodes_edges().nodes} initialEdges={nodes_edges().edges} />
             </Grid>
-            <Grid container spacing={1} justifyContent={"space-between"}>
-
-              <Grid item xs={12} sm={4}>
-                <FormControl size='small' fullWidth>
-                  <InputLabel>Desde</InputLabel>
-                  <Select
-                    label='Desde'
-                    value={tipo}
-                    onChange={handle_change}
-                  >
-                    {tipo_desde.map(({ value, label }) => (
-                      <MenuItem key={value} value={value}>
-                        {label}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>
-              </Grid>
-
-              <Grid item xs={12} sm={4}>
-                <FormControl size='small' fullWidth>
-                  <InputLabel>Hasta</InputLabel>
-                  <Select
-                    label='Hasta'
-                    value={tipo_hasta}
-                    onChange={handle_change_hasta}
-                  >
-                    {tipo_desde.map(({ value, label }) => (
-                      <MenuItem key={value} value={value}>
-                        {label}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>
-              </Grid>
+            <Grid container spacing={0} justifyContent={"space-between"}>
               <Stack
                 justifyContent="flex-end"
               >
                 <Button
                   color="success"
                   variant="contained"
+                  onClick={() => {
+                    set_add_flujo_proceso(true);
+                  }}
                 >
-                  Agregar
+                  Agregar nuevo flujo
                 </Button>
               </Stack>
             </Grid>
           </Box>
         </Grid>
       </Grid>
+      <AddFlujoProcesoModal
+        is_modal_active={add_flujo_proceso}
+        set_is_modal_active={set_add_flujo_proceso}
+        etapas_proceso={etapas_proceso}
+        form_data_flujo={form_data_flujo}
+        handle_input_change={handle_input_change}
+        handle_select_change={handle_select_change}
+        handle_submit={handle_submit}
+      />
     </>
   )
 }
