b505b5d15f1adb512fa1e0c37c23ccee54ff0401
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-05-10 10:43:34 -0500

    Modifiqué los handles para que estén posicionados al top y al bottom de cada nodo.

--- src/commons/recaudo/components/flowChart/CustomNode/FlowNode.js
+++ src/commons/recaudo/components/flowChart/CustomNode/FlowNode.js
@@ -4,20 +4,22 @@
 import { Handle, Position } from "reactflow";
 import './styles.css';
 
-export const FlowNode = ({ data }) => {
+export const FlowNode = ({ data, isConnectable }) => {
   // const onChange = useCallback((evt) => {
   //   console.log(evt.target.value);
   // }, []);
 
   return (
     <div className="custom-node-wrapper">
-      <Handle type="target" position={Position.Top} id="a" />
+      <Handle type="target" position={Position.Top} id="top" isConnectable={isConnectable} />
+      {/* <Handle type="target" position={Position.Left} id="left" isConnectable={isConnectable} /> */}
       <div>
         <p>{data.nombre}</p>
         <p>{data.fecha}</p>
         <p>{data.descripcion}</p>
       </div>
-      <Handle type="source" position={Position.Right} id="b" />
+      <Handle type="source" position={Position.Bottom} id="bottom" isConnectable={isConnectable} />
+      {/* <Handle type="source" position={Position.Right} id="right" isConnectable={isConnectable} /> */}
     </div>
   );
 };
