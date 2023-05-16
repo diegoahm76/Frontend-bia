1166368e1e57c1a87bc5cc650d6502ff222d66a4
Elver Rodriguez <adelson9602@gmail.com>
2023-05-15 19:12:35 -0500

    Se quita condicional para hacer registro, la condicional era de prueba

--- src/commons/auth/hooks/registerPersonaNaturalHook.tsx
+++ src/commons/auth/hooks/registerPersonaNaturalHook.tsx
@@ -232,15 +232,12 @@ export const use_register_persona_n = ({
     try {
       values.redirect_url =
         'https://macareniafrontendevelopv2.netlify.app/#/auth/activacion_cuenta';
-      const is_send = false;
-      if (is_send) {
-        const { data } = await crear_persona_natural_and_user(
-          values as DataRegisterPersonaN
-        );
-        control_success(data.detail);
+      const { data } = await crear_persona_natural_and_user(
+        values as DataRegisterPersonaN
+      );
+      control_success(data.detail);
 
-        // window.location.href = '#/app/auth/login';
-      }
+      window.location.href = '#/app/auth/login';
     } catch (error) {
       const temp_error = error as AxiosError;
       const resp = temp_error.response?.data as ResponseServer<any>;
