ca5f51218c6f5aef0ac69a3527a59646c22ed36a
macareniacorp <macareniacorpsvillavicencio@hotmail.com>
2023-05-25 15:52:07 -0500

    Cambio Url Axios https://back-end-bia-beta.up.railway.app/api/

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://backend-bia-beta-production.up.railway.app/api/'
+  baseURL: 'https://back-end-bia-beta.up.railway.app/api/'
 });
 
 api.interceptors.request.use(
