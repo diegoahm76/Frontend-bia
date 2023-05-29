cc94a30679c326168ad592f27e13407df84ed0ca
AlejoSasto <sastoque42@gmail.com>
2023-05-28 19:58:01 -0500

    Inicio Cambio de contrasaña

--- src/commons/seguridad/screens/DatosAccesoScreen.tsx
+++ src/commons/seguridad/screens/DatosAccesoScreen.tsx
@@ -1,11 +1,38 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Box, Button, Grid, Stack, TextField } from '@mui/material';
 import { Title } from '../../../components';
 import { CambioContrasenaModal } from '../components/datosAcceso/CambioContrasenaModal';
+import { control_error } from '../../../helpers';
+import type { AxiosError } from 'axios';
+import { useSelector } from 'react-redux';
+import type { AuthSlice } from '../../auth/interfaces';
+import { get_user_by_id } from '../../../request/getRequest';
+import type { Users } from '../interfaces';
+
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const DatosAccesoScreen: React.FC = () => {
   const [datos_acceso, set_datos_acceso] = useState<boolean>(false);
+  const [datos, set_datos] = useState<Users>();
+  const { userinfo } = useSelector((state: AuthSlice) => state.auth);
+
+  const on_result = async (id_usuario: number): Promise<void> => {
+    try {
+      const {
+        data: { data },
+      } = await get_user_by_id(id_usuario);
+      set_datos(data)
+    } catch (err) {
+      const temp = err as AxiosError;
+      if (temp.response?.status !== 404) {
+        control_error(err);
+      }
+    }
+  };
+
+  useEffect(() => {
+    void on_result(userinfo.id_usuario);
+  }, []);
 
   return (
     <>
@@ -31,17 +58,29 @@ export const DatosAccesoScreen: React.FC = () => {
             <Grid container>
               <Grid item container spacing={2} xs={12} sm={4}>
                 <Grid item xs={12} sm={12}>
-                  <TextField label="Nombre de usuario" size="small" fullWidth />
+                  <TextField label="Nombre de usuario"
+                    size="small"
+                    disabled
+                    value={datos?.nombre_de_usuario}
+                    fullWidth
+                  />
                 </Grid>
                 <Grid item xs={12} sm={12}>
-                  <TextField label="Tipo usuario" size="small" fullWidth />
+                  <TextField label="Tipo usuario"
+                    size="small"
+                    fullWidth
+                    disabled
+                    value={datos?.tipo_usuario}
+                  />
                 </Grid>
                 <Grid item xs={12} sm={12}>
                   <TextField
-                    // label="fecha activacion inicial"
+                    label="fecha activacion inicial"
                     type="date"
                     size="small"
                     fullWidth
+                    disabled
+                    value={datos?.fecha_ultimo_cambio_activacion}
                   />
                 </Grid>
                 <Stack justifyContent="center" sx={{ p: '20px' }}>
