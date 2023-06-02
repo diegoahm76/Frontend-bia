c9577a9b7cea0a6a22c50a3beb1e9a4981b2073b
John J Rodriguez <87795271+JohnRodriguezM@users.noreply.github.com>
2023-06-02 13:20:51 -0500

    home screen

--- src/commons/home/portada/Jaguar.tsx
+++ src/commons/home/portada/Jaguar.tsx
@@ -1,7 +1,39 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/naming-convention */
 // eslint-disable-next-line @typescript-eslint/naming-convention
+
+import {motion} from 'framer-motion';
+
+/* const styles = {
+  maxHeight: '298px',
+  width: '100%',
+  height: '220%',
+  marginLeft: '-20px',
+  marginTop: '-120px',
+} */
+
+
+
 export const Jaguar: React.FC = () => {
   return (
-    <img src='../image/imagenes/Jaguar.png' style={{ width: '510px', height: '330px', marginTop: '-200px' }}
+    <motion.div
+      animate={{
+        scale: [1, 1.2, 1.2, 1, 1],
+      }}
+      transition={{
+        duration: 2,
+      }}
+    >
+    <img src='../image/imagenes/Jaguar.png' style={{
+  maxHeight: '298px',
+  // width: '100%',
+  height: '220%',
+  position: 'absolute',
+  zIndex: 1,
+  left: '-10px',
+  bottom: '-5px  ',
+}}
     />
+    </motion.div>
   );
 }
