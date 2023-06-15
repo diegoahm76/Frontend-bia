40181f9d66d04c29182b43a3eef8aa40fe5151b7
John J Rodriguez <87795271+JohnRodriguezM@users.noreply.github.com>
2023-06-15 13:24:34 -0500

    se añade context para el manejo de modales del ccd, se configuran y corren las pruebas necesarias para el context, se crean los hooks para el manejo de los maodales

---
+++ src/commons/gestorDocumental/ccd/context/ModalContext.tsx
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/no-confusing-void-expression */
+/* eslint-disable @typescript-eslint/naming-convention */
+import { createContext, useState, useCallback } from 'react';
+import { type ModalContextState } from '../interfaces/ModalInterfaces/Modal.types';
+
+const ModalContext = createContext<ModalContextState | undefined>(undefined);
+
+const ModalProvider: React.FC<any> = ({ children }: any) => {
+  const [modal1, setModal1] = useState(false);
+  const [modal2, setModal2] = useState(false);
+  const [modal3, setModal3] = useState(false);
+
+  const openModal1 = useCallback(() => setModal1(true), []);
+  const closeModal1 = useCallback(() => setModal1(false), []);
+  const openModal2 = useCallback(() => setModal2(true), []);
+  const closeModal2 = useCallback(() => setModal2(false), []);
+  const openModal3 = useCallback(() => setModal3(true), []);
+  const closeModal3 = useCallback(() => setModal3(false), []);
+
+  return (
+    <ModalContext.Provider
+      value={{
+        modal1,
+        modal2,
+        modal3,
+        openModal1,
+        closeModal1,
+        openModal2,
+        closeModal2,
+        openModal3,
+        closeModal3,
+      }}
+    >
+      {children}
+    </ModalContext.Provider>
+  );
+};
+
+export { ModalContext, ModalProvider };
