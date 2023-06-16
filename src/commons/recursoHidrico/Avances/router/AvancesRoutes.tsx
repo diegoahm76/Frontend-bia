f87fa7f66c34ef1b9ebe4a24079cbba5d99f96b1
AlejoSasto <sastoque42@gmail.com>
2023-06-16 18:43:36 -0500

    Se inicia entrega 26, se realiza 2 modales de busqueda avanzada y se registra de manera temporal un avance

--- src/commons/recursoHidrico/Avances/router/AvancesRoutes.tsx
+++ src/commons/recursoHidrico/Avances/router/AvancesRoutes.tsx
@@ -1,15 +1,15 @@
 import type { ReactElement } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Page404 } from '../../../../screens/404';
-import { PorhMainScreen } from '../screen/PorhMainScreen/PorhMainScreen';
 import { UserProvider } from '../context/contextData';
+import { AvanceScreen } from '../screen/AvencesScreen/AvancesScreen';
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
-export const PorhRoutes = (): ReactElement => {
+export const AvancesRoutes = (): ReactElement => {
   return (
     <UserProvider>
     <Routes>
-      <Route path="contenido_programatico/*" element={<PorhMainScreen/>} />
+      <Route path="avances/*" element={<AvanceScreen/>} />
       <Route path="/*" element={<Page404 />} />
     </Routes>
     </UserProvider>
