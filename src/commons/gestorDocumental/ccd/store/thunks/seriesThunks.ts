0edd9ab10bdd125eac78e6aab31c4cfed831e98c
John J Rodriguez <87795271+JohnRodriguezM@users.noreply.github.com>
2023-06-17 21:52:53 -0500

    Revert "John"

--- src/commons/gestorDocumental/ccd/store/thunks/seriesThunks.ts
+++ src/commons/gestorDocumental/ccd/store/thunks/seriesThunks.ts
@@ -6,7 +6,7 @@ import { api } from '../../../../../api/axios';
 import { type AxiosError, type AxiosResponse } from 'axios';
 // Reducers
 import { get_series_ccd } from '../slices/seriesSlice';
-import { control_error, control_success } from '../../utils/success_errors';
+import { control_error, control_success } from './ccdThunks';
 // Interfaces
 // import { type ISeriesObject } from '../../interfaces/ccd';
 
