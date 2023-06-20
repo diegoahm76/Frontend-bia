d0b10a2f072614df5cee6e5c253ff9e7683ee3b1
HectorStiven <132477892+HectorStiven@users.noreply.github.com>
2023-06-20 17:08:12 -0500

    actualizar color de boton de descargar pdf

--- src/commons/conservacion/gestorVivero/screens/AdministrarViveroScreen.tsx
+++ src/commons/conservacion/gestorVivero/screens/AdministrarViveroScreen.tsx
@@ -404,8 +404,7 @@ export function AdministrarViveroScreen(): JSX.Element {
 
   const button_style = {
     color: 'white',
-    backgroundColor: '#335B1E',
-    border: '3px solid black',
+   
     borderRadius: '50%',
     width: '40px',
     height: '40px',
@@ -445,11 +444,11 @@ export function AdministrarViveroScreen(): JSX.Element {
           </Stack>
 
           <ButtonGroup style={{ margin: 7 }}  >
-            <Button style={button_style} onClick={export_to_excel}>
+            <Button style={{ ...button_style, backgroundColor: '#335B1E' }} onClick={export_to_excel}>
               <i className="pi pi-file-excel"></i>
             </Button>
 
-            <Button style={button_style} onClick={export_pdf}>
+            <Button style={{ ...button_style, backgroundColor: 'red' }} onClick={export_pdf}>
               <i className="pi pi-file-pdf"></i>
             </Button>
 
