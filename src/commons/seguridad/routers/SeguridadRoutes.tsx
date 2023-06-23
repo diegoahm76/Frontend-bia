216326c3d1de425a48113417edfd391b69dd932c
miguel-73 <miguelangelmurciaocampo73@gmail.com>
2023-06-23 18:09:09 -0500

    style creaccion de pantalla de sucursal 23/06

--- src/commons/seguridad/routers/SeguridadRoutes.tsx
+++ src/commons/seguridad/routers/SeguridadRoutes.tsx
@@ -8,12 +8,15 @@ import { Page404 } from '../../../screens/404';
 import { AuditoriaScreen } from '../screens/AuditoriaScreen/AuditoriaScreen';
 import { DelegacionSuperuserScreen } from '../screens/DelegacionSuperuserScreen';
 import { ConfiguracionesBasicasRoutes } from '../screens/ConfiguracionesBasicas/Routes/ConfiguracionesBasicas';
+import { SucursalEntidad } from '../components/SucursalEntidad/SucursalEntidad';
+// import  from '../components/SucursalEntidad/SucursalEntidad';
 
 const routes = [
   { path: '/auditoria', element: <AuditoriaScreen /> },
   { path: '/roles', element: <RolesScreen /> },
   { path: '/administracion_usuarios', element: <AdministracionPersonasScreen /> },
   { path: '/delegacion_superusuario', element: <DelegacionSuperuserScreen /> },
+  { path: '/sucursal_entidad', element: <SucursalEntidad /> },
 
 ];
 export const SeguridadRoutes: React.FC = () => {
