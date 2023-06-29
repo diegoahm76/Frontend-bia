0154ce1b2516948e8e7bb8a12e8901dac5953de1
HectorStiven <132477892+HectorStiven@users.noreply.github.com>
2023-06-29 16:57:09 -0500

    AJUSTAR modal de paantalla

--- src/commons/seguridad/components/ConfiguracionEntidad/components/Editables/MostrarEditables.tsx
+++ src/commons/seguridad/components/ConfiguracionEntidad/components/Editables/MostrarEditables.tsx
@@ -8,8 +8,10 @@ import { ModalEditarCargo } from "./ModalEditable/MotadlEditable";
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const MostrarEditables: React.FC = () => {
 
-
-
+// eslint-disable-next-line @typescript-eslint/naming-convention
+    const director = "stiven";
+    const fecha = "hoy";
+ 
 
     return (
         <Grid container sx={{
@@ -26,19 +28,18 @@ export const MostrarEditables: React.FC = () => {
                 <Grid item container spacing={7}>
 
                     <Grid item xs={12} sm={6} >
-                        <Typography variant="subtitle1" fontWeight="bold">Director</Typography>
-                        <InputText aria-describedby="username-help" disabled placeholder="Nombre completo" />
-                        <ModalEditarCargo />
-                        <br /> <br />
-                        <label>Registrado desde</label>
+                     
+                        <Typography  variant="subtitle1" fontWeight="bold">Director</Typography>
+                      <InputText aria-describedby="username-help" disabled placeholder="Nombre completo" />
+                        <ModalEditarCargo name={director} fecha={fecha} titlee={"Director"}/>                   
+                      <label>Registrado desde</label>
                         <InputText type="text" className="p-inputtext-sm" placeholder="19/08/2000" style={{ margin: 3, height: 10, width: "30%" }} />
                     </Grid>
 
                     <Grid item xs={12} sm={6} >
                         <Typography variant="subtitle1" fontWeight="bold">Coordinador de Almacen</Typography>
-                        <InputText aria-describedby="username-help" disabled placeholder="Nombre completo" />
-                        <ModalEditarCargo />
-                        <br /> <br />
+                        <InputText   aria-describedby="username-help" disabled placeholder="Nombre completo" />
+                        <ModalEditarCargo name={director} fecha={fecha} titlee={"Coordinador de Almacen"} />
                         <label>Registrado desde</label>
                         <InputText type="text" className="p-inputtext-sm" placeholder="19/08/2000" style={{ margin: 3, height: 10, width: "30%" }} />
                     </Grid>
@@ -47,8 +48,8 @@ export const MostrarEditables: React.FC = () => {
                     <Grid item xs={12} sm={6} >
                         <Typography variant="subtitle1" fontWeight="bold">Coordinador de Viveros</Typography>
                         <InputText aria-describedby="username-help" disabled placeholder="Nombre completo" />
-                        <ModalEditarCargo />
-                        <br /> <br />
+                        <ModalEditarCargo name={director} fecha={fecha} titlee={"Coordinador de Viveros"} />
+                      
                         <label>Registrado desde</label>
                         <InputText type="text" className="p-inputtext-sm" placeholder="19/08/2000" style={{ margin: 3, height: 10, width: "30%" }} />
                     </Grid>
@@ -57,8 +58,8 @@ export const MostrarEditables: React.FC = () => {
                     <Grid item xs={12} sm={6} >
                         <Typography variant="subtitle1" fontWeight="bold">Coordinador de Transporte</Typography>
                         <InputText aria-describedby="username-help" disabled placeholder="Nombre completo" />
-                        <ModalEditarCargo />
-                        <br /> <br />
+                        <ModalEditarCargo name={director} fecha={fecha} titlee={"Coordinador de Transporte"} /> 
+                       
                         <label>Registrado desde</label>
                         <InputText type="text" className="p-inputtext-sm" placeholder="19/08/2000" style={{ margin: 3, height: 10, width: "30%" }} />
                     </Grid>
@@ -67,8 +68,8 @@ export const MostrarEditables: React.FC = () => {
                     <Grid item xs={12} sm={6} >
                         <Typography variant="subtitle1" fontWeight="bold">Almacenista</Typography>
                         <InputText aria-describedby="username-help" disabled placeholder="Nombre completo" />
-                        <ModalEditarCargo />
-                        <br /> <br />
+                        <ModalEditarCargo name={director} fecha={fecha} titlee={"Almacenista"} /> 
+                        
                         <label>Registrado desde</label>
                         <InputText type="text" className="p-inputtext-sm" placeholder="19/08/2000" style={{ margin: 3, height: 10, width: "30%" }} />
                     </Grid>
