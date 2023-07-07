a8a9d47e1c8d75e8d000d0cf40b1083cd1d676a2
AlejoSasto <sastoque42@gmail.com>
2023-07-07 18:55:12 -0500

    Se agregan maketacion de instrumentos

--- src/commons/recursoHidrico/ConsultaBiblioteca/screen/ConsultaBibliotecaSreen.tsx
+++ src/commons/recursoHidrico/ConsultaBiblioteca/screen/ConsultaBibliotecaSreen.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import {
   Avatar,
   Divider,
@@ -11,10 +12,14 @@ import { DataGrid, type GridColDef } from '@mui/x-data-grid';
 import ChecklistIcon from '@mui/icons-material/Checklist';
 import { useContext, useEffect } from 'react';
 import { DataContext } from '../context/contextData';
+import { v4 as uuidv4 } from 'uuid';
 import '../css/styles.css';
+import { DownloadButton } from '../../../../utils/DownloadButton/DownLoadButton';
+import { ButtonSalir } from '../../../../components/Salir/ButtonSalir';
+import { LoadingButton } from '@mui/lab';
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
-export const ConsultaBibliotecaSreen: React.FC = () => {
+export const ConsultaBibliotecaSreen: React.FC = (): JSX.Element => {
   const columns_seccion: GridColDef[] = [
     {
       field: 'nombre',
@@ -112,12 +117,46 @@ export const ConsultaBibliotecaSreen: React.FC = () => {
       ),
     },
   ];
+  const columns_cuencas: GridColDef[] = [
+    {
+      field: 'ubicacion',
+      headerName: 'UBICACIÓN',
+      width: 300,
+    },
+    {
+      field: 'nombre',
+      headerName: 'NOMBRE',
+      width: 300,
+      renderCell: (params) => <div className="container">{params.value}</div>,
+    },
+  ];
+
+  const columns_anexos: GridColDef[] = [
+    {
+      field: 'nombre',
+      headerName: 'NOMBRE',
+      width: 300,
+    },
+    {
+      field: 'archivo',
+      headerName: 'ARCHIVO',
+      width: 200,
+      renderCell: (params) => (
+        <DownloadButton
+          fileUrl={params.value}
+          fileName={params.row.nombre}
+          condition={false}
+        />
+      ),
+    },
+  ];
 
   const {
     id_seccion,
     rows_seccion,
     rows_subseccion,
     info_seccion,
+    info_subseccion,
     set_id_seccion,
     set_info_seccion,
     set_id_subseccion,
@@ -134,84 +173,212 @@ export const ConsultaBibliotecaSreen: React.FC = () => {
     void fetch_data_subseccion_por_seccion();
   }, [id_seccion]);
 
+  const info_instrumento = {
+    nombre: 'PORH RIO GUATAQUIA',
+    cuencas: [
+      {
+        ubicacion: 'CUNDINAMARCA',
+        nombre: 'CUENCA RIO GUATAQUIA',
+      },
+      {
+        ubicacion: 'CUNDINAMARCA',
+        nombre: 'CUENCA RIO GUATAQUIA 2',
+      },
+    ],
+    fecha_vigencia: '01/10/2022',
+    resolucion: 'P.J-7.20-03-2008',
+    anexos: [
+      {
+        nombre: 'ANEXO 1',
+        archivo:
+          'https://image.slidesharecdn.com/laresolucin-140622160742-phpapp02/85/la-resolucin-3-320.jpg?cb=1665748181',
+      },
+      {
+        nombre: 'ANEXO 2',
+        archivo:
+          'https://static.docsity.com/documents_first_pages/2019/09/20/df0c20aa9b53e9265f863197def9b536.png',
+      },
+    ],
+  };
+
   return (
-    <Grid
-      container
-      spacing={2}
-      m={2}
-      p={2}
-      sx={{
-        position: 'relative',
-        background: '#FAFAFA',
-        borderRadius: '15px',
-        p: '20px',
-        m: '10px 0 20px 0',
-        mb: '20px',
-        boxShadow: '0px 3px 6px #042F4A26',
-      }}
-    >
-      <Grid item xs={12}>
-        <Title title="VISUALIZACIÓN BIBLIOTECA" />
-      </Grid>
-      {rows_seccion.length > 0 && (
-        <>
-          <Grid item xs={12}>
-            <Typography variant="subtitle1" fontWeight="bold">
-              Listado de secciones existentes
-            </Typography>
-            <Divider />
-          </Grid>
-          <Grid item xs={12}>
-            <DataGrid
-              autoHeight
-              rows={rows_seccion}
-              columns={columns_seccion}
-              getRowId={(row) => row.id_seccion}
-              pageSize={5}
-              rowsPerPageOptions={[5]}
-              rowHeight={80}
-            />
-          </Grid>
-        </>
-      )}
-      {rows_subseccion.length > 0 && (
-        <>
+    <>
+      <Grid
+        container
+        spacing={2}
+        m={2}
+        p={2}
+        sx={{
+          position: 'relative',
+          background: '#FAFAFA',
+          borderRadius: '15px',
+          p: '20px',
+          m: '10px 0 20px 0',
+          mb: '20px',
+          boxShadow: '0px 3px 6px #042F4A26',
+        }}
+      >
         <Grid item xs={12}>
-        <Title title="SUBSECCIONES" />
+          <Title title="VISUALIZACIÓN BIBLIOTECA" />
+        </Grid>
+        {rows_seccion.length > 0 && (
+          <>
+            <Grid item xs={12}>
+              <Typography variant="subtitle1" fontWeight="bold">
+                Listado de secciones existentes
+              </Typography>
+              <Divider />
+            </Grid>
+            <Grid item xs={12}>
+              <DataGrid
+                autoHeight
+                rows={rows_seccion}
+                columns={columns_seccion}
+                getRowId={(row) => row.id_seccion}
+                pageSize={5}
+                rowsPerPageOptions={[5]}
+                rowHeight={80}
+              />
+            </Grid>
+          </>
+        )}
+        {rows_subseccion.length > 0 && (
+          <>
+            <Grid item xs={12}>
+              <Title title="SUBSECCIONES" />
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="subtitle1" fontWeight="bold">
+                Sección seleccionada:
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                size="small"
+                disabled
+                fullWidth
+                value={info_seccion?.nombre}
+                id="disabled-field"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="subtitle1" fontWeight="bold">
+                Listado de Subsecciones existentes
+              </Typography>
+              <Divider />
+            </Grid>
+            <Grid item xs={12}>
+              <DataGrid
+                autoHeight
+                rows={rows_subseccion}
+                columns={columns_subseccion}
+                getRowId={(row) => row.id_subseccion}
+                pageSize={5}
+                rowsPerPageOptions={[5]}
+                rowHeight={80}
+              />
+            </Grid>
+          </>
+        )}
+        {info_subseccion && (
+          <>
+            <Grid item xs={12}>
+              <Title title="INSTRUMENTOS" />
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="subtitle1" fontWeight="bold">
+                Subsección seleccionada:
+              </Typography>
+              <Divider />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                size="small"
+                disabled
+                fullWidth
+                value={info_subseccion?.nombre}
+                id="disabled-field"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="subtitle1" fontWeight="bold">
+                Detalle de instrumento seleccionado:
+              </Typography>
+              <Divider />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                label="Nombre del instrumento"
+                size="small"
+                disabled
+                fullWidth
+                value={info_instrumento?.nombre}
+                id="disabled-field"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                label="Fecha de vigencia"
+                size="small"
+                disabled
+                fullWidth
+                value={info_instrumento?.fecha_vigencia}
+                id="disabled-field"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                label="Resolución"
+                size="small"
+                disabled
+                fullWidth
+                value={info_instrumento?.resolucion}
+                id="disabled-field"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}></Grid>
+            <Grid item xs={12} sm={6}>
+              <Typography variant="subtitle1" fontWeight="bold">
+                Cuencas Asociadas:
+              </Typography>
+              <Divider />
+              <DataGrid
+                autoHeight
+                rows={info_instrumento.cuencas}
+                columns={columns_cuencas}
+                getRowId={(row) => uuidv4()}
+                pageSize={5}
+                rowsPerPageOptions={[5]}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Typography variant="subtitle1" fontWeight="bold">
+                Anexos:
+              </Typography>
+              <Divider />
+              <DataGrid
+                autoHeight
+                rows={info_instrumento.anexos}
+                columns={columns_anexos}
+                getRowId={(row) => uuidv4()}
+                pageSize={5}
+                rowsPerPageOptions={[5]}
+              />
+            </Grid>
+          </>
+        )}
       </Grid>
-          <Grid item xs={12}>
-            <Typography variant="subtitle1" fontWeight="bold">
-              Sección seleccionada:
-            </Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              size="small"
-              disabled
-              fullWidth
-              value={info_seccion?.nombre}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Typography variant="subtitle1" fontWeight="bold">
-              Listado de Subsecciones existentes
-            </Typography>
-            <Divider />
-          </Grid>
-          <Grid item xs={12}>
-            <DataGrid
-              autoHeight
-              rows={rows_subseccion}
-              columns={columns_subseccion}
-              getRowId={(row) => row.id_subseccion}
-              pageSize={5}
-              rowsPerPageOptions={[5]}
-              rowHeight={80}
-            />
-          </Grid>
-        </>
-      )}
-      
-    </Grid>
+      <Grid container justifyContent="flex-end" spacing={2}>
+        <Grid item>
+          <LoadingButton variant="contained" color="primary">
+          Buscar
+          </LoadingButton>
+        </Grid>
+
+        <Grid item>
+          <ButtonSalir />
+        </Grid>
+      </Grid>
+    </>
   );
 };
