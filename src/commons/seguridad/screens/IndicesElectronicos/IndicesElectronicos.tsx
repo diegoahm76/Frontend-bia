9798e7f3a16c20818fc2d2e00cf4b0c146205e19
John J Rodriguez <87795271+JohnRodriguezM@users.noreply.github.com>
2023-07-07 18:52:53 -0500

    xmlAdvance

--- src/commons/seguridad/screens/IndicesElectronicos/IndicesElectronicos.tsx
+++ src/commons/seguridad/screens/IndicesElectronicos/IndicesElectronicos.tsx
@@ -3,7 +3,51 @@
 import { type FC, useEffect, useState } from 'react';
 import { api } from '../../../../api/axios';
 import { v4 as uuidv4 } from 'uuid';
+import { DataGrid, type GridColDef } from '@mui/x-data-grid';
+import { Avatar, IconButton } from '@mui/material';
+import { AvatarStyles } from '../../../gestorDocumental/ccd/componentes/crearSeriesCcdDialog/utils/constant';
+import EditIcon from '@mui/icons-material/Edit';
 export const IndicesElectronicos: FC = (): JSX.Element => {
+  const columns_indices_electronicos: GridColDef[] = [
+    {
+      headerName: 'NOMBRE',
+      field: 'nombre',
+      minWidth: 200,
+      maxWidth: 225,
+      flex: 1
+    },
+    {
+      headerName: 'searchIndex',
+      field: 'searchIndex',
+      minWidth: 250,
+      maxWidth: 200,
+      flex: 1
+    },
+    {
+      headerName: 'Acciones',
+      field: 'accion',
+      minWidth: 150,
+      maxWidth: 170,
+      flex: 1,
+      renderCell: (params: any) => (
+        <>
+          <IconButton
+            onClick={() => {
+              console.log('params edit formato', params.row);
+            }}
+          >
+            <Avatar sx={AvatarStyles} variant="rounded">
+              <EditIcon
+                titleAccess="Editar formato tipo de medio"
+                sx={{ color: 'primary.main', width: '18px', height: '18px' }}
+              />
+            </Avatar>
+          </IconButton>
+        </>
+      )
+    }
+  ];
+
   const [data, setData] = useState<any>([]);
 
   useEffect(() => {
@@ -11,7 +55,7 @@ export const IndicesElectronicos: FC = (): JSX.Element => {
     void api.get(url).then((response) => {
       const newData = response.data.map((item: any) => ({
         ...item,
-        searchIndex: uuidv4()
+        searchIndex: uuidv4().slice(0, 8)
       }));
       console.log(newData);
 
@@ -22,6 +66,15 @@ export const IndicesElectronicos: FC = (): JSX.Element => {
   return (
     <div>
       <h1>Hola desde los índices electronicos</h1>
+      <DataGrid
+        density="compact"
+        autoHeight
+        columns={columns_indices_electronicos}
+        rows={data}
+        getRowId={(row) => row.searchIndex}
+        pageSize={5}
+        rowsPerPageOptions={[10]}
+      />
     </div>
   );
 };
