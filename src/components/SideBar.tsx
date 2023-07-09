b701fd9be93fcac144fbd303452d5ad85b8ebe2f
John J Rodriguez <87795271+JohnRodriguezM@users.noreply.github.com>
2023-07-08 21:34:10 -0500

    logout session confirmed button

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -31,6 +31,7 @@ import { logout } from '../commons/auth/store';
 import { SuperUserScreen } from '../commons/seguridad/screens/SuperUserScreen';
 import { FooterGov } from '../components/goviernoEnLinea/FooterGov';
 import { HeaderGov } from '../components/goviernoEnLinea/HeaderGov';
+import Swal from 'sweetalert2';
 
 interface Props {
   window?: () => Window;
@@ -253,9 +254,22 @@ export const SideBar: React.FC<Props> = ({ window, drawer_width }: Props) => {
             )}
             <ListItemButton
               sx={{ pl: 4 }}
-              onClick={() => {
-                dispatch(logout(''));
-              }}
+                onClick={() => {
+                  void Swal.fire({
+                    
+                    title: '¿Está seguro de cerrar sesión?',
+                    showDenyButton: true,
+                    confirmButtonText: `Si`,
+                    denyButtonText: `No`,
+                  }).then((result) => {
+                    /* Read more about isConfirmed, isDenied below */
+                    if (result.isConfirmed) {
+                      dispatch(logout(''));
+                    } else if (result.isDenied) {
+                      void Swal.fire('No se ha cerrado sesión', '', 'info')
+                    }
+                  })
+                }}
             >
               <ListItemIcon>
                 <LogoutIcon sx={{ color: mod_dark ? '#fafafa' : '#141415' }} />
