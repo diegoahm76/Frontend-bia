81384f76fd6488c9a8350e6ecce1626ea185af3f
GDiana <dcquinche@gmail.com>
2023-07-12 19:39:16 -0500

    Feat Maquetado Pantalla Recibo de Pago

--- src/commons/recaudo/facilidadPago/screens/ReciboPago.tsx
+++ src/commons/recaudo/facilidadPago/screens/ReciboPago.tsx
@@ -1,8 +1,20 @@
+import { Stack } from '@mui/material';
 import { Title } from '../../../../components/Title';
+import { ReciboPagoModulo } from '../componentes/ReciboPago/ReciboPago';
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const ReciboPago: React.FC = () => {
   return (
-    <Title title='Generar Recibo de Pago' />
+    <>
+      <Title title='Generar Recibo de Pago' />
+      <Stack
+        direction="row"
+        justifyContent="center"
+        spacing={2}
+        sx={{ mt: '30px' }}
+      >
+        <ReciboPagoModulo />
+      </Stack>
+    </>
   )
 }
