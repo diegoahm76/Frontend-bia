687bedd43fcc178c1b24e4edb1740cd72c053e59
GDiana <dcquinche@gmail.com>
2023-07-20 13:33:41 -0500

    Modulo Direccion de Entrega en Notificaciones

--- src/commons/recaudo/facilidadPago/notificaciones/componentes/ModuloDireccion.tsx
+++ src/commons/recaudo/facilidadPago/notificaciones/componentes/ModuloDireccion.tsx
@@ -1,215 +1,199 @@
-import { useState } from 'react';
-import { Grid, TextField, Typography, Button } from '@mui/material';
-import { LoadingButton } from '@mui/lab';
-import { CustomSelect } from '../../../../../components/CustomSelect';
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import { useEffect, useState } from 'react';
+import { Box, Button, FormControl, Grid, InputLabel, MenuItem, Select, TextField } from '@mui/material';
+import { type IList } from '../../../../../interfaces/globalModels';
+import { type event } from '../../interfaces/interfaces';
+import { get_paises, get_departamentos, get_ciudades } from '../../../../../request/getRequest';
 import { DialogGeneradorDeDirecciones } from '../../../../../components/DialogGeneradorDeDirecciones';
-import type { PropsRegister } from '../../../../../interfaces/globalModels';
-import { use_register_persona_n } from '../../../../auth/hooks/registerPersonaNaturalHook';
-import type { PropsElement } from '../../../../auth/interfaces/authModels';
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
-const DatosResidencia: (props: PropsElement) => JSX.Element = ({
-  errors,
-  watch,
-  setValue: set_value,
-  getValues,
-  register,
-  handleBack,
-  typeDirection,
-}: PropsElement) => {
-  const {
-    paises_options,
-    dpts_residencia_opt,
-    ciudades_residencia_opt,
-    municipio_residencia,
-    pais_residencia,
-    departamento_residencia,
-    on_change,
-  } = use_register_persona_n({ watch, setValue: set_value, getValues });
-  const direccion = watch('direccion_residencia') ?? '';
-  const generate_direction = (): void => {
-    if (typeDirection !== undefined) {
-      typeDirection('residencia');
-    }
-  };
-  return (
-    <>
-      <Grid container spacing={2} mt={0.1}>
-        <Grid item xs={12} sm={6} md={4}>
-          <CustomSelect
-            onChange={on_change}
-            label="País de residencia *"
-            name="pais_residencia"
-            value={pais_residencia}
-            options={paises_options}
-            required={true}
-            errors={errors}
-            register={register}
-          />
-        </Grid>
-        {pais_residencia === 'CO' && (
-          <>
-            <Grid item xs={12} sm={6} md={4}>
-              <CustomSelect
-                onChange={on_change}
-                label="Departamento *"
-                name="departamento_residencia"
-                value={departamento_residencia}
-                options={dpts_residencia_opt}
-                required={true}
-                errors={errors}
-                register={register}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6} md={4}>
-              <CustomSelect
-                onChange={on_change}
-                label="Ciudad *"
-                name="municipio_residencia"
-                value={municipio_residencia}
-                options={ciudades_residencia_opt}
-                disabled={departamento_residencia === '' ?? true}
-                required={true}
-                errors={errors}
-                register={register}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6} md={4}>
-              <TextField
-                size="small"
-                label="Direccion *"
-                disabled
-                fullWidth
-                error={errors.direccion_residencia?.type === 'required'}
-                helperText={
-                  errors.direccion_residencia?.type === 'required'
-                    ? 'Este campo es obligatorio'
-                    : ''
-                }
-                {...register('direccion_residencia', {
-                  required: true,
-                })}
-                value={direccion}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6} md={4}>
-              <Button variant="contained" onClick={generate_direction}>
-                Generar dirección
-              </Button>
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                size="small"
-                type="textarea"
-                rows="3"
-                label="Complemento dirección"
-                {...register('direccion_residencia_ref')}
-              />
-            </Grid>
-          </>
-        )}
+export const ModuloDireccion: React.FC = () => {
+  const [paises_options, set_paises_options] = useState<IList[]>([]);
+  const [departamentos_options, set_departamentos_options] = useState<IList[]>([]);
+  const [ciudades_options, set_ciudades_options] = useState<IList[]>([]);
+  const [pais_selected, set_pais_selected] = useState('');
+  const [departamento_selected, set_departamento_selected] = useState('');
+  const [ciudad_selected, set_ciudad_selected] = useState('');
+  const [direccion, set_direccion] = useState('');
+  const [complemento, set_complemento] = useState('');
+  const [modal, set_modal] = useState(false);
 
-        {/* BOTONES */}
-        <Grid item spacing={2} justifyContent="end" container>
-          <Grid item>
-            <Button
-              variant="contained"
-              color="primary"
-              sx={{
-                textAlign: 'center',
-                color: '#ffff',
-              }}
-              href="#/auth/login"
-            >
-              <Typography sx={{ color: 'white' }}>Inicio</Typography>
-            </Button>
+  console.log(ciudad_selected, complemento)
+
+  const handle_close = () => { set_modal(false) }
+  const handle_open = () => { set_modal(true) }
+
+  const get_lista_paises = async (): Promise<void> => {
+    try {
+      const { data: { data: res_paises } } = await get_paises();
+      set_paises_options(res_paises ?? []);
+    } catch (error: any) {
+      throw new Error(error);
+    }
+  }
+
+  const get_lista_departamentos = async (pais: string): Promise<void> => {
+    try {
+      const { data: { data } } = await get_departamentos(pais);
+      set_departamentos_options(data ?? []);
+    } catch (error: any) {
+      throw new Error(error);
+    }
+  }
+
+  const get_lista_ciudades = async (departamento: string): Promise<void> => {
+    try {
+      const { data: { data } } = await get_ciudades(departamento);
+      set_ciudades_options(data ?? []);
+    } catch (error: any) {
+      throw new Error(error);
+    }
+  }
+
+  const get_direccion_modal = (value: string): void => {
+    set_direccion(value);
+  };
+
+  useEffect(() => {
+    void get_lista_paises();
+  }, [])
+
+  useEffect(() => {
+    void get_lista_departamentos(pais_selected);
+  }, [pais_selected])
+
+  useEffect(() => {
+    void get_lista_ciudades(departamento_selected);
+  }, [departamento_selected])
+
+  return (
+    <Grid
+      container
+      sx={{
+        position: 'relative',
+        background: '#FAFAFA',
+        borderRadius: '15px',
+        mb: '20px',
+        mt: '20px',
+        p: '20px',
+        boxShadow: '0px 3px 6px #042F4A26',
+      }}
+    >
+      <Grid item xs={12}>
+        <Box
+          component="form"
+          noValidate
+          autoComplete="off"
+        >
+          <h3>Dirección de Entrega</h3>
+          <Grid container spacing={2} mb='20px'>
+            <Grid item xs={12} sm={6} md={4}>
+              <FormControl required fullWidth size='small'>
+                <InputLabel>País de Residencia</InputLabel>
+                <Select
+                  size='small'
+                  label="País de residencia"
+                  defaultValue={''}
+                  onChange={(event: event) => {
+                    const { value } = event.target
+                    set_pais_selected(value)
+                  }}
+                >
+                  {
+                    paises_options.map((pais) => (
+                      <MenuItem key={pais.value} value={pais.value}>{pais.label}</MenuItem>
+                    ))
+                  }
+                </Select>
+              </FormControl>
+            </Grid>
+            {
+              pais_selected === 'CO' ? (
+                <>
+                  <Grid item xs={12} sm={6} md={4}>
+                    <FormControl required fullWidth size='small'>
+                      <InputLabel>Departamento</InputLabel>
+                      <Select
+                        size='small'
+                        label="Departamento"
+                        defaultValue={''}
+                        onChange={(event: event) => {
+                          const { value } = event.target
+                          set_departamento_selected(value)
+                        }}
+                      >
+                        {
+                          departamentos_options.map((departamento) => (
+                            <MenuItem key={departamento.value} value={departamento.value}>{departamento.label}</MenuItem>
+                          ))
+                        }
+                      </Select>
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={12} sm={6} md={4}>
+                    <FormControl required fullWidth size='small'>
+                      <InputLabel>Ciudad</InputLabel>
+                      <Select
+                        size='small'
+                        label="Ciudad"
+                        defaultValue={''}
+                        disabled={departamento_selected === ''}
+                        onChange={(event: event) => {
+                          const { value } = event.target
+                          set_ciudad_selected(value)
+                        }}
+                      >
+                        {
+                          ciudades_options.map((ciudad) => (
+                            <MenuItem key={ciudad.value} value={ciudad.value}>{ciudad.label}</MenuItem>
+                          ))
+                        }
+                      </Select>
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={12} sm={6} md={4}>
+                    <TextField
+                      required
+                      size="small"
+                      label="Dirección"
+                      disabled
+                      fullWidth
+                      value={direccion}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6} md={4}>
+                    <Button
+                      variant="contained"
+                      onClick={handle_open}
+                    >
+                      Generar Dirección
+                    </Button>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      fullWidth
+                      size="small"
+                      type="textarea"
+                      rows="3"
+                      label="Complemento Dirección"
+                      onChange={(event: event) => {
+                        const { value } = event.target
+                        set_complemento(value)
+                      }}
+                    />
+                  </Grid>
+                </>
+              ) : null
+            }
           </Grid>
-          <Grid item>
-            <Button variant="outlined" color="error" onClick={handleBack}>
-              Volver
-            </Button>
-          </Grid>
-          <Grid item>
-            <LoadingButton
-              type="submit"
-              variant="contained"
-              fullWidth
-              color="primary"
-            >
-              Continuar
-            </LoadingButton>
-          </Grid>
-        </Grid>
+        </Box>
       </Grid>
-    </>
-  );
-};
-
-// eslint-disable-next-line @typescript-eslint/naming-convention
-export const ModuloDireccion: React.FC<PropsRegister> = ({
-  register,
-  handleSubmit: handle_submit,
-  setValue: set_value,
-  errors,
-  isValid: is_valid,
-  watch,
-  getValues,
- }: PropsRegister) => {
-  const { is_modal_active, on_submit, set_value_direction, open_modal } =
-    use_register_persona_n({ watch, setValue: set_value, getValues });
-
-  const [type_direction, set_type_direction] = useState('');
-  const [active_step, set_active_step] = useState(0);
-
-  const send_data = handle_submit((e) => {
-    if (active_step === 5 && is_valid) {
-      void on_submit(e);
-    } else {
-      handle_next();
-    }
-  });
-
-  // Paso siguiente del stepper
-  const handle_next = (): void => {
-    if (active_step !== 5) {
-      set_active_step((prevActiveStep) => prevActiveStep + 1);
-    }
-  };
-
-  // Paso anterior del stepper
-  const handle_back = (): void => {
-    set_active_step((prevActiveStep) => prevActiveStep - 1);
-  };
-
-  const on_direction = (type: string): void => {
-    set_type_direction(type);
-    open_modal(true);
-  };
-
-  return (
-    <>
-      <form
-        onSubmit={(e) => {
-          void send_data(e);
-        }}
-      >
-        <DatosResidencia
-          errors={errors}
-          getValues={getValues}
-          register={register}
-          setValue={set_value}
-          watch={watch}
-          handleBack={handle_back}
-          typeDirection={on_direction}
-        />
-      </form>
       <DialogGeneradorDeDirecciones
-        open={is_modal_active}
-        openDialog={open_modal}
-        onChange={set_value_direction}
-        type={type_direction}
+        open={modal}
+        openDialog={handle_close}
+        onChange={get_direccion_modal}
+        type='residencia'
       />
-    </>
-  );
-};
+    </Grid>
+  )
+}
