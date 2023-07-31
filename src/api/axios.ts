dc8ec6a65b4820a0c757444da7e529130e0d1a2f
DevMacarenia <redes@macarenia.org>
2023-07-31 16:20:05 -0500

    Cambios back-end-bia-preproduction.up.railway.app

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
-const baseURL = "https://back-end-bia-beta.up.railway.app/api/"
+const baseURL = "https://back-end-bia-preproduction.up.railway.app/api/"
 // const baseURL = "http://70.30.6.237/api/"
 export const api = axios.create({
   baseURL,
