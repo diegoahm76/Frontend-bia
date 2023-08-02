bfc71b508c754613b04aec72809df9bc14405cfd
John J Rodriguez <87795271+JohnRodriguezM@users.noreply.github.com>
2023-08-02 11:52:25 -0500

    .

---
+++ src/commons/Transversales/modules/corporativo/screens/LideresXUnidadOrganizacional/screens/LideresXUnidadOrganizacionalMainScreen.tsx
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+
+export const LideresXUnidadOrganizacionalMainScreen = (): JSX.Element => {
+  return (
+    <div>LideresXUnidadOrganizacionalMainScreen</div>
+  )
+}
