b322de4b49adbf02b39311c3ccae4b573d432d8d
John J Rodriguez <87795271+JohnRodriguezM@users.noreply.github.com>
2023-08-01 21:29:55 -0500

    traslado msivo entrega 29

--- src/commons/Transversales/modules/procesos/routes/ProcesosRoutes.tsx
+++ src/commons/Transversales/modules/procesos/routes/ProcesosRoutes.tsx
@@ -1,24 +1,21 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import { Route, Routes } from 'react-router-dom';
 import { Page404 } from '../../../../../screens/404';
+import { OrganigramaScreen } from '../../../../gestorDocumental/organigrama/screens/OrganigramaScreen';
+import { TrasladoMasivoUnidadOrganizacional } from '../screens/TrasladoMasivoUnidadOrganizacional/screens/TrasladoMasivoUnidadOrganizacional';
 
-
-
-/*
 const routes = [
-  { path: '/datos_restringidos', element: <ActualizacionDatosRestringidosScreen /> },
-  { path: '/administracion_personas', element: <AdminUsuariosScreen /> },
-  { path: '/autorizacion_notificacion', element: <NotificacionPage /> },
-]; */
+  { path: '/traslado_masivo_unidad_por_entidad', element: <TrasladoMasivoUnidadOrganizacional/> },
+  { path: '/traslado_masivo_unidad_a_unidad', element: <>Traslado masivo de unidad a unidad</> },
+  { path: '/cambio_organigrama_actual', element: <OrganigramaScreen /> },
+];
+
 export const ProcesosRoutes: React.FC = () => {
   return (
     <Routes>
-     {/* {routes.map((route, index) => (
+      {routes.map((route, index) => (
         <Route key={index} path={route.path} element={route.element} />
-      ))} */}
-      <Route path="/traslado_masivo_unidad_por_entidad" element={<>  Traslado masico unidad por entidad </> } />
-      <Route path="/traslado_masivo_unidad_a_unidad" element={<>  Traslado masivo de unidad a unidad </> } />
-      
+      ))}
       <Route path="/*" element={<Page404 />} />
     </Routes>
   );
--- src/commons/Transversales/routes/TransversalRoutes.tsx
+++ src/commons/Transversales/routes/TransversalRoutes.tsx
@@ -3,16 +3,22 @@ import { Route, Routes } from 'react-router-dom';
 import { Page404 } from '../../../screens/404';
 import { VinculacionColaboradoresRoutes } from '../../seguridad/screens/vinculacionColaboradores/routes/VinculacionColaboradores';
 import { NotificacionPage } from '../../seguridad/screens/NotificacionPage';
-import { ActualizacionDatosRestringidosScreen, AdminUsuariosScreen } from '../../seguridad/screens';
+import {
+  ActualizacionDatosRestringidosScreen,
+  AdminUsuariosScreen
+} from '../../seguridad/screens';
 import { ProcesosRoutes } from '../modules/procesos/routes/ProcesosRoutes';
 import { ConfiguracionEntidad } from '../../seguridad/components/ConfiguracionEntidad/screens/configuracionEntidad';
-
+import { CorporativoRoutes } from '../modules/corporativo/routes/CorporativoRoutes';
 
 const routes = [
-  { path: '/datos_restringidos', element: <ActualizacionDatosRestringidosScreen /> },
+  {
+    path: '/datos_restringidos',
+    element: <ActualizacionDatosRestringidosScreen />
+  },
   { path: '/administracion_personas', element: <AdminUsuariosScreen /> },
   { path: '/autorizacion_notificacion', element: <NotificacionPage /> },
-  { path: '/configuracion_entidad', element: <ConfiguracionEntidad /> },
+  { path: '/configuracion_entidad', element: <ConfiguracionEntidad /> }
 ];
 export const TransversalRoutes: React.FC = () => {
   return (
@@ -20,8 +26,12 @@ export const TransversalRoutes: React.FC = () => {
       {routes.map((route, index) => (
         <Route key={index} path={route.path} element={route.element} />
       ))}
-      <Route path="vinculacion_colaboradores/*" element={<VinculacionColaboradoresRoutes />} />
+      <Route
+        path="vinculacion_colaboradores/*"
+        element={<VinculacionColaboradoresRoutes />}
+      />
       <Route path="procesos/*" element={<ProcesosRoutes />} />
+      <Route path="corporativo/*" element={<CorporativoRoutes />} />
       <Route path="/*" element={<Page404 />} />
     </Routes>
   );
