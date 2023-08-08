10948bde87db27169629cc569abd34a619919df0
DevMacarenia <redes@macarenia.org>
2023-08-08 11:11:40 -0500

    Cambio de ruta

--- src/commons/auth/api/auth.ts
+++ src/commons/auth/api/auth.ts
@@ -1,3 +1,3 @@
-export const auth_url = "https://cormacarenabiamaster.netlify.app/#"
+export const auth_url = "https://biadevmacarenia.netlify.app/#"
 // export const auth_url = "http://70.30.6.237/#"
 
