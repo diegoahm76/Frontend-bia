5653d37a74b0d7e0833e7404e8b62f2b8af27896
DevMacarenia <redes@macarenia.org>
2023-08-09 10:46:50 -0500

    desarrollo

--- src/commons/auth/api/auth.ts
+++ src/commons/auth/api/auth.ts
@@ -1,4 +1,5 @@
 
-export const auth_url = "https://cormacarenabiamaster.netlify.app/#"
+export const auth_url = "https://biacormacarenamaster.netlify.app/#"
+// export const auth_url = "https://biacormacarenamaster.netlify.app/#"
 // export const auth_url = "http://70.30.6.237/#"
 
