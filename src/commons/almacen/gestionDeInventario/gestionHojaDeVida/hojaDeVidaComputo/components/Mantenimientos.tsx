416253f016e7c64a0397aee698562e636fcf9432
Lorena Martínez Pineda <118283887+sloremart@users.noreply.github.com>
2023-08-10 11:33:56 -0500

    cerraar

---
+++ src/commons/almacen/gestionDeInventario/gestionHojaDeVida/hojaDeVidaComputo/components/Mantenimientos.tsx
@@ -0,0 +1,88 @@
+
+import { Box, Grid } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
+import type { GridColDef } from '@mui/x-data-grid';
+import { Title } from '../../../../../../components/Title';
+import { get_maintenance } from '../store/thunks/cvComputoThunks';
+import { useAppDispatch, useAppSelector } from '../../../../../../hooks';
+
+
+
+// eslint-disable-next-line @typescript-eslint/naming-convention, @typescript-eslint/explicit-function-return-type
+const Mantenimiento = () => {
+
+    const { current_cv_computer, maintenance } = useAppSelector((state) => state.cv);
+    const dispatch = useAppDispatch();
+    void dispatch(get_maintenance(current_cv_computer.id_articulo ?? 0)
+    );
+
+    const columns_mantenimientos: GridColDef[] = [
+
+        {
+            field: 'tipo_descripcion',
+            headerName: 'Tipo de mantenimiento',
+            width: 250,
+            cellClassName: 'truncate-cell'
+
+        },
+        {
+            field: 'fecha',
+            headerName: 'Fecha de mantenimiento',
+            width: 250,
+            cellClassName: 'truncate-cell'
+
+        },
+        {
+            field: 'estado',
+            headerName: 'Estado',
+            width: 250,
+            cellClassName: 'truncate-cell'
+
+        },
+        {
+            field: 'responsable',
+            headerName: 'Responsable',
+            width: 250,
+            cellClassName: 'truncate-cell'
+
+        },
+
+    ];
+
+
+
+    return (
+        <>
+            <Grid container direction="row" padding={2} borderRadius={2}>
+                <Grid
+                    container
+                    spacing={2}
+                    justifyContent="center"
+                    direction="row"
+                    marginTop={2}
+                >
+                    <Box sx={{ width: '100%' }}>
+                        <Title title="Mantenimientos programados" />
+                        <DataGrid
+                            density="compact"
+                            autoHeight
+                            columns={columns_mantenimientos}
+                            pageSize={10}
+                            rowsPerPageOptions={[10]}
+                            experimentalFeatures={{ newEditingApi: true }}
+                            getRowId={(row) => row.id_programacion_mantenimiento}
+                            rows={maintenance}
+                        />
+
+                    </Box>
+                </Grid>
+            </Grid>
+        </>
+    );
+};
+
+// eslint-disable-next-line no-restricted-syntax
+export default Mantenimiento;
+
+
+
