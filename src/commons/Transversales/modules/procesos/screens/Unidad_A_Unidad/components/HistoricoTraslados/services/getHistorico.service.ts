f8ae71f69e8d9cb746dc5e7ac5f059e31da6e1dd
John J Rodriguez <87795271+JohnRodriguezM@users.noreply.github.com>
2023-08-10 17:52:42 -0500

    nigth

--- src/commons/Transversales/modules/procesos/screens/Unidad_A_Unidad/components/HistoricoTraslados/services/getHistorico.service.ts
+++ src/commons/Transversales/modules/procesos/screens/Unidad_A_Unidad/components/HistoricoTraslados/services/getHistorico.service.ts
@@ -1,17 +1,17 @@
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 /* eslint-disable @typescript-eslint/naming-convention */
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
-import { api } from "../../../../../../../../../api/axios";
-import { control_error } from "../../../../../../../../../helpers";
-
+import { api } from '../../../../../../../../../api/axios';
+import {
+  control_error,
+  control_success
+} from '../../../../../../../../../helpers';
 
 export const getHistoricoTrasladosU_U = async (): Promise<any> => {
   try {
     const url = 'transversal/organigrama/historico-unidad-unidad/';
     const { data } = await api.get(url);
-   /* control_success(
-      "Se encontraron los siguientes TRD's usados" || data.detail
-    ); */
+    control_success(data.detail || 'Se encontró el siguiente histórico');
     return data.data;
   } catch (error: any) {
     control_error(
