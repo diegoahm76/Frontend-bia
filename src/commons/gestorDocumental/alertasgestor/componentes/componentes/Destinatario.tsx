4c51d7daba153b121f96c7b63d70ddf60ec6bc9d
miguel-73 <miguelangelmurciaocampo73@gmail.com>
2023-08-19 18:46:57 -0500

    alertas , configuarando crear

--- src/commons/gestorDocumental/alertasgestor/componentes/componentes/Destinatario.tsx
+++ src/commons/gestorDocumental/alertasgestor/componentes/componentes/Destinatario.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+/* eslint-disable @typescript-eslint/naming-convention */
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
-import { FormControl, Grid, InputLabel, MenuItem, Select, IconButton, Button, } from '@mui/material';
+import { FormControl, Grid, InputLabel, MenuItem, Select, IconButton, Button, TextField, } from '@mui/material';
 import type { SelectChangeEvent } from '@mui/material';
 import { Title } from '../../../../../components';
 import DeleteIcon from '@mui/icons-material/Delete';
@@ -13,7 +15,13 @@ import { Alertas, Persona } from '../../interfaces/types';
 import { api } from '../../../../../api/axios';
 import SaveIcon from '@mui/icons-material/Save';
 
-
+export interface AlertaPersona {
+    id_persona_alertar: number | null;
+    perfil_sistema: string | null;
+    cod_clase_alerta: string;
+    id_persona: number | null;
+    id_unidad_org_lider: number | null;
+}
 export interface SelectItem {
     value: string;
     label: string;
@@ -96,7 +104,6 @@ export const Destinatario: React.FC = () => {
             ),
         },
     ];
-
     const [perfil, set_perfil] = useState<SelectItem[]>([]);
     const handleeliminafila = async (): Promise<void> => {
         if (selected_row) {
@@ -130,6 +137,53 @@ export const Destinatario: React.FC = () => {
     const handleperfil = (event: SelectChangeEvent<string>): void => {
         setselec_perfil(event.target.value);
     };
+    const [selected_button, setselected_button] = useState<string | null>(null);
+    const handle_selectlider = (): void => {
+        setselected_button('lider');
+    };
+    const handle_selectperfil = (): void => {
+        setselected_button('perfil');
+    };
+    const handle_selectbuscar = (): void => {
+        setselected_button('buscador');
+    };
+
+    // crear 
+    const initialFormData = {
+        id_persona_alertar: null,
+        perfil_sistema: null,
+        cod_clase_alerta: '',
+        id_persona: null,
+        id_unidad_org_lider: null,
+    };
+
+    const [formData, setFormData] = useState(initialFormData);
+
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    const handleInputChange = (event: { target: { name: any; value: any; }; }) => {
+        const { name, value } = event.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+            
+        }));
+    };
+
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    const handleSubmit = async (event: { preventDefault: () => void; }) => {
+        event.preventDefault();
+
+        try {
+            const response = await api.post('/transversal/alertas/personas_alertar/create/', formData);
+            console.log('Alerta de persona creada exitosamente:', response.data);
+            // Reset form data after successful submission
+            setFormData(initialFormData);
+        } catch (error) {
+            console.error('Error al crear la alerta de persona:', error);
+        }
+    };
+
+    
     return (
         <Grid container
             spacing={2}
@@ -138,61 +192,92 @@ export const Destinatario: React.FC = () => {
                 position: 'relative',
                 background: '#FAFAFA',
                 borderRadius: '15px',
-                p: '20px',
-                m: '10px 0 20px 0',
+                p: '20px', m: '10px 0 20px 0',
                 mb: '20px',
                 boxShadow: '0px 3px 6px #042F4A26',
             }}
         >
+            <form onSubmit={handleSubmit}>
+                <TextField
+                    label="Código de Clase de Alerta"
+                    variant="outlined"
+                    fullWidth
+                    name="cod_clase_alerta"
+                    value={formData.cod_clase_alerta}
+                    onChange={handleInputChange}
+                    required
+                />
+                <TextField
+                    label="ID de Persona"
+                    variant="outlined"
+                    fullWidth
+                    type="number"
+                    name="id_persona"
+                    value={formData.id_persona}
+                    onChange={handleInputChange}
+                    required
+                />
+                {/* You can add more TextField components for other fields */}
 
-
+                <Button type="submit" variant="contained" color="primary">
+                    Crear Alerta de Persona
+                </Button>
+            </form>
             <Grid item marginTop={-2} xs={12}>
                 <Title title="Destinatario" />
             </Grid>
-            <Grid item xs={12} sm={3}>
-                <FormControl fullWidth size="small">
-                    <InputLabel  >Lider de unidad</InputLabel>
-                    <Select
-                        label="Lider de unidad"
-                    >
-                        <MenuItem value="Ten">Ten</MenuItem>
-                        <MenuItem value="Twenty">Twenty</MenuItem>
-                    </Select>
-                </FormControl>
-            </Grid>
-            <Grid item xs={12} sm={3}>
-                <FormControl fullWidth size="small">
-                    <InputLabel  >Unidad grupo rentas</InputLabel>
-                    <Select
-                        label="Unidad grupo rentas"
-                    >
-                        <MenuItem value="Ten">Ten</MenuItem>
-                        <MenuItem value="Twenty">Twenty</MenuItem>
-                    </Select>
-                </FormControl>
-            </Grid>
-            <Grid item xs={12} sm={3}>
-                <FormControl fullWidth size="small">
-                    <InputLabel>Perfil</InputLabel>
-                    <Select value={selec_perfil} label="Perfil" onChange={handleperfil}>
-                        {perfil.map(item => (
-                            <MenuItem key={item.value} value={item.value}>
-                                {item.label}
-                            </MenuItem>
-                        ))}
-                    </Select>
-                </FormControl>
+
+            <Grid item xs={12}>
+                <Button onClick={handle_selectlider}>  Lider de unidad</Button>
+                <Button onClick={handle_selectperfil}>  Perfil</Button>
+                <Button onClick={handle_selectbuscar}>  BuscadorPersona</Button>
             </Grid>
 
+            {selected_button === 'lider' && (
+                <Grid item xs={12}>
 
-            <Grid item xs={12} >
+                    <Grid item xs={12} sm={3}>
+                        <FormControl fullWidth size="small">
+                            <InputLabel>Lider de unidad</InputLabel>
+                            <Select label="Lider de unidad">
+                                <MenuItem value="Ten">Ten</MenuItem>
+                                <MenuItem value="Twenty">Twenty</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Grid>
+                </Grid>
+            )}
 
-                <BuscadorPersona
-                    onResult={(data) => {
-                        void on_result(data);
-                    }}
-                />
-            </Grid>
+            {selected_button === 'perfil' && (
+                <Grid item xs={12}>
+
+
+                    <Grid item xs={12} sm={3}>
+                        <FormControl fullWidth size="small">
+                            <InputLabel>Perfil</InputLabel>
+                            <Select value={selec_perfil} label="Perfil" onChange={handleperfil}>
+                                {perfil.map(item => (
+                                    <MenuItem key={item.value} value={item.value}>
+                                        {item.label}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                    </Grid>
+                </Grid>
+
+            )}
+
+            {selected_button === 'buscador' && (
+                <Grid item xs={12}>
+                    <BuscadorPersona
+                        onResult={(data) => {
+                            void on_result(data);
+                        }}
+                    />
+                </Grid>
+            )}
+
             <Grid item>
                 < Button
                     variant="contained"
@@ -205,6 +290,7 @@ export const Destinatario: React.FC = () => {
                 </Button>
             </Grid>
             <>{persona?.primer_nombre}</>
+            <>{persona?.id_persona}</>
             <Grid item xs={12}  >
                 <DataGrid
                     density="compact"
