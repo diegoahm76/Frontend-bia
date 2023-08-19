fbd68e72ae52af004e8970bcdac014e8cefd411e
John J Rodriguez <87795271+JohnRodriguezM@users.noreply.github.com>
2023-08-19 11:51:55 -0500

    .

--- src/commons/Transversales/modules/procesos/screens/Unidad_Por_Entidad/screen/pruebaScreen.tsx
+++ src/commons/Transversales/modules/procesos/screens/Unidad_Por_Entidad/screen/pruebaScreen.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import { useContext, type FC } from 'react';
 import { ContextUnidadxEntidad } from '../context/ContextUnidadxEntidad';
+import { ModalHistoricoTraslados } from '../components/ModalHistoricoTraslado/screen/ModalHistoricoTraslados';
 
 export const pruebaScreen: FC = (): JSX.Element => {
   const { handleModalHistoricos } = useContext(ContextUnidadxEntidad);
@@ -14,6 +15,8 @@ export const pruebaScreen: FC = (): JSX.Element => {
       >
         Open Modal historivos
       </button>
+
+      <ModalHistoricoTraslados />
     </div>
   );
 };
