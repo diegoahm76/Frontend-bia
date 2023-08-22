e7315d02eb52666d5d27e987ffd67d25288cbfeb
AlejoSasto <sastoque42@gmail.com>
2023-08-22 09:34:21 -0500

    Se crean rutas de depositos

--- src/commons/gestorDocumental/routes/GestorDocumentalRoutes.tsx
+++ src/commons/gestorDocumental/routes/GestorDocumentalRoutes.tsx
@@ -5,52 +5,40 @@ import { TrdRoutes } from '../trd/routes/TrdRoutes';
 import { TcaRoutes } from '../tca/routes/TcaRoutes';
 import { VentanillaRoutes } from '../ventanilla/routes/VentanillaRoutes';
 import { Page404 } from '../../../screens/404';
-import DepositoScreen from '../deposito/screens/depositoScreen';
+import { DepositosRoutes } from '../deposito/router/DepositosRoutes';
 
 const routes = [
   {
-    path: "organigrama/",
-    name: "organigrama",
-    component: () => (
-      <OrganigramaRoutes />
-    ),
+    path: 'organigrama/',
+    name: 'organigrama',
+    component: () => <OrganigramaRoutes />,
   },
   {
-    path: "ccd/",
-    name: "ccd",
-    component: () => (
-      <CcdRoutes />
-    ),
+    path: 'ccd/',
+    name: 'ccd',
+    component: () => <CcdRoutes />,
   },
   {
-    path: "trd/",
-    name: "trd",
-    component: () => (
-      <TrdRoutes />
-    ),
+    path: 'trd/',
+    name: 'trd',
+    component: () => <TrdRoutes />,
   },
   {
-    path: "tca/",
-    name: "tca",
-    component: () => (
-      <TcaRoutes />
-    ),
+    path: 'tca/',
+    name: 'tca',
+    component: () => <TcaRoutes />,
   },
   {
-    path: "deposito/",
-    name: "deposito",
-    component: () => (
-      <DepositoScreen />
-    ),
+    path: 'archivo/',
+    name: 'archivo',
+    component: () => <DepositosRoutes />,
   },
   {
-    path: "ventanilla_unica/",
-    name: "ventanilla_unica",
-    component: () => (
-      <VentanillaRoutes />
-    ),
+    path: 'ventanilla_unica/',
+    name: 'ventanilla_unica',
+    component: () => <VentanillaRoutes />,
   },
-]
+];
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const GestorDocumentalRoutes: React.FC = () => {
   return (
@@ -58,7 +46,7 @@ export const GestorDocumentalRoutes: React.FC = () => {
       {routes.map((route) => (
         <Route
           key={route.path}
-          path={`${route.path}/${route.path === "/" ? "" : "*"}`}
+          path={`${route.path}/${route.path === '/' ? '' : '*'}`}
           element={route.component()}
         />
       ))}
