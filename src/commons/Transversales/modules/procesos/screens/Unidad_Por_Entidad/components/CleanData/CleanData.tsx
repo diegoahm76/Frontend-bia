7384c440407b883c22ca033ec3a0a89cc05a55d2
John J Rodriguez <87795271+JohnRodriguezM@users.noreply.github.com>
2023-08-22 10:49:17 -0500

    cambios en entrega29

--- src/commons/Transversales/modules/procesos/screens/Unidad_Por_Entidad/components/CleanData/CleanData.tsx
+++ src/commons/Transversales/modules/procesos/screens/Unidad_Por_Entidad/components/CleanData/CleanData.tsx
@@ -1,11 +1,17 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import { type FC } from 'react';
+import { type FC, useContext } from 'react';
 import { containerStyles } from '../../../../../../../gestorDocumental/tca/screens/utils/constants/constants';
 import { Button, Grid, Stack } from '@mui/material';
 import { Title } from '../../../../../../../../components';
 import CleanIcon from '@mui/icons-material/CleaningServices';
+import { ContextUnidadxEntidad } from '../../context/ContextUnidadxEntidad';
+import { ModalHistoricoTraslados } from '../ModalHistoricoTraslado/screen/ModalHistoricoTraslados';
 
 export const CleanData: FC<any> = (): JSX.Element => {
+  //* elements from context
+
+  const { handleModalHistoricos } = useContext(ContextUnidadxEntidad);
+
   return (
     <>
       <Grid container sx={containerStyles}>
@@ -51,14 +57,10 @@ export const CleanData: FC<any> = (): JSX.Element => {
                     color="primary"
                     variant="outlined"
                     startIcon={<CleanIcon />}
-                    onClick={() => {
-                      // eslint-disable-next-line no-console
-                      console.log('cleaning fields');
-                    }}
+                    onClick={handleModalHistoricos}
                   >
-                    REINICIAR CAMPOS
+                    VER HISTÓRICO DE TRASLADOS MASIVOS
                   </Button>
-
 
                   <Button
                     color="primary"
@@ -77,6 +79,15 @@ export const CleanData: FC<any> = (): JSX.Element => {
           </form>
         </Grid>
       </Grid>
+
+      {/*  modal historico traslados masivos, solo debe estar en la opción numero 2
+        Traslado de unidades organizacionales de organigrama anterior a actual
+      
+                    SE DEBE HABILITAR LA CONDICIONAL DE MUESTRA O NO DEPDENDIENDO EL ESCENARIO (MODE)
+
+      */}
+
+      <ModalHistoricoTraslados />
     </>
   );
 };
