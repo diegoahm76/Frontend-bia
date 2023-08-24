0fafa4d8750d707791819fc223540cf6d3e43075
Palebaro <barpadroid@gmail.com>
2023-08-24 14:03:27 -0500

    Se integran servicios y vista de historicos (Entrega 48)

--- src/commons/conservacion/Reportes/routes/ReportesRoutes.tsx
+++ src/commons/conservacion/Reportes/routes/ReportesRoutes.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import { Page404 } from '../../../../screens/404';
 import { SubsistemaConservacionScreen } from '../screens/SubsistemaConservacion';
+import { HistoricoMovimientosScreen } from '../screens/HistoricoMovimientos';
 ;
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
@@ -9,6 +10,7 @@ export const ReportesRoutes: React.FC = () => {
     
     <Routes>
       <Route path="subsistema" element={<SubsistemaConservacionScreen/>} />
+      <Route path="historicos" element={<HistoricoMovimientosScreen/>} />
       <Route path="/*" element={<Page404 />} />
     </Routes>
   );
