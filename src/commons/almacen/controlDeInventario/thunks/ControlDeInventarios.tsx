0f3c08345e990d222c33423867411fc7aa14e62f
Palebaro <barpadroid@gmail.com>
2023-09-27 14:22:19 -0500

    Se realiza entrega 74

--- src/commons/almacen/controlDeInventario/thunks/ControlDeInventarios.tsx
+++ src/commons/almacen/controlDeInventario/thunks/ControlDeInventarios.tsx
@@ -56,10 +56,10 @@ export const obtener_estados: any = () => {
   };
 };
 // Obtiene inventario activos fijos
-export const obtener_inventario_af: any = (filtros: {seleccion_bodega: string, seleccion_estado: string, seleccion_ubicacion: string, seleccion_propiedad: string, seleccion_categoria: string, bienes_baja: boolean, bienes_salida: boolean, seleccion_origen: string}) => {
+export const obtener_inventario_af: any = (filtros: { seleccion_bodega: string, seleccion_estado: string, seleccion_ubicacion: string, seleccion_propiedad: string, seleccion_categoria: string, bienes_baja: boolean, bienes_salida: boolean, seleccion_origen: string }) => {
   return async () => {
     try {
-      const { data } = await api.get(`almacen/inventario/control/activos-fijos/get-list/?id_bodega=${filtros.seleccion_bodega === 'Todos' ? '': filtros.seleccion_bodega}&cod_estado_activo=${filtros.seleccion_estado === 'Todos' ? '': filtros.seleccion_estado}&ubicacion=${filtros.seleccion_ubicacion === 'Todos' ? '': filtros.seleccion_ubicacion}&propiedad=${filtros.seleccion_propiedad === 'Todos' ? '': filtros.seleccion_propiedad}&cod_tipo_activo=${filtros.seleccion_categoria === 'Todos' ? '': filtros.seleccion_categoria}&realizo_baja=${filtros.bienes_baja}&realizo_salida=${filtros.bienes_salida}&cod_tipo_entrada=${filtros.seleccion_origen === 'Todos' ? '': filtros.seleccion_origen}`);
+      const { data } = await api.get(`almacen/inventario/control/activos-fijos/get-list/?id_bodega=${filtros.seleccion_bodega === 'Todos' ? '' : filtros.seleccion_bodega}&cod_estado_activo=${filtros.seleccion_estado === 'Todos' ? '' : filtros.seleccion_estado}&ubicacion=${filtros.seleccion_ubicacion === 'Todos' ? '' : filtros.seleccion_ubicacion}&propiedad=${filtros.seleccion_propiedad === 'Todos' ? '' : filtros.seleccion_propiedad}&cod_tipo_activo=${filtros.seleccion_categoria === 'Todos' ? '' : filtros.seleccion_categoria}&realizo_baja=${filtros.bienes_baja}&realizo_salida=${filtros.bienes_salida}&cod_tipo_entrada=${filtros.seleccion_origen === 'Todos' ? '' : filtros.seleccion_origen}`);
       return data;
     } catch (error: any) {
       control_error(error.response.data.detail);
@@ -80,10 +80,10 @@ export const obtener_bien_especifico_af: any = (seleccion_bien: string) => {
   };
 };
 // Obtiene inventario según categoría
-export const obtener_inventario_categoria: any = (filtros: {seleccion_bodega: string, seleccion_categoria: string}) => {
+export const obtener_inventario_categoria: any = (filtros: { seleccion_bodega: string, seleccion_categoria: string }) => {
   return async () => {
     try {
-      const { data } = await api.get(`almacen/inventario/control/activos-fijos/get-by-categoria/?id_bodega=${filtros.seleccion_bodega === 'Todos' ? '': filtros.seleccion_bodega}&cod_tipo_activo=${filtros.seleccion_categoria === 'Todos' ? '': filtros.seleccion_categoria}`);
+      const { data } = await api.get(`almacen/inventario/control/activos-fijos/get-by-categoria/?id_bodega=${filtros.seleccion_bodega === 'Todos' ? '' : filtros.seleccion_bodega}&cod_tipo_activo=${filtros.seleccion_categoria === 'Todos' ? '' : filtros.seleccion_categoria}`);
       return data;
     } catch (error: any) {
       control_error(error.response.data.detail);
@@ -92,10 +92,10 @@ export const obtener_inventario_categoria: any = (filtros: {seleccion_bodega: st
   };
 };
 // Obtiene inventario propio
-export const obtener_inventario_propio: any = (filtros: {seleccion_bodega: string, seleccion_categoria: string, agrupar: boolean}) => {
+export const obtener_inventario_propio: any = (filtros: { seleccion_bodega: string, seleccion_categoria: string, agrupar: boolean }) => {
   return async () => {
     try {
-      const { data } = await api.get(`almacen/inventario/control/activos-fijos/get-propio/?id_bodega=${filtros.seleccion_bodega === 'Todos' ? '': filtros.seleccion_bodega}&cod_tipo_activo=${filtros.seleccion_categoria === 'Todos' ? '': filtros.seleccion_categoria}&agrupado=${filtros.agrupar}`);
+      const { data } = await api.get(`almacen/inventario/control/activos-fijos/get-propio/?id_bodega=${filtros.seleccion_bodega === 'Todos' ? '' : filtros.seleccion_bodega}&cod_tipo_activo=${filtros.seleccion_categoria === 'Todos' ? '' : filtros.seleccion_categoria}&agrupado=${filtros.agrupar}`);
       return data;
     } catch (error: any) {
       control_error(error.response.data.detail);
@@ -104,10 +104,10 @@ export const obtener_inventario_propio: any = (filtros: {seleccion_bodega: strin
   };
 };
 // Obtiene inventario por tipo
-export const obtener_inventario_tipo: any = (filtros: {seleccion_bodega: string, seleccion_categoria: string, mostrar: boolean}) => {
+export const obtener_inventario_tipo: any = (filtros: { seleccion_bodega: string, seleccion_categoria: string, mostrar: boolean }) => {
   return async () => {
     try {
-      const { data } = await api.get(`almacen/inventario/control/activos-fijos/get-by-tipo/?id_bodega=${filtros.seleccion_bodega === 'Todos' ? '': filtros.seleccion_bodega}&resultados_entidad=${filtros.mostrar}`);
+      const { data } = await api.get(`almacen/inventario/control/activos-fijos/get-by-tipo/?id_bodega=${filtros.seleccion_bodega === 'Todos' ? '' : filtros.seleccion_bodega}&resultados_entidad=${filtros.mostrar}`);
       return data;
     } catch (error: any) {
       control_error(error.response.data.detail);
@@ -115,11 +115,35 @@ export const obtener_inventario_tipo: any = (filtros: {seleccion_bodega: string,
     }
   };
 };
-// Obtiene bienes
+// Obtiene todo el inventario para bienes de consumo
+export const obtener_inventario_consumo: any = (filtros: { seleccion_bodega: string, seleccion_bien: string, solicitable: boolean | string, agrupar_bodega: boolean }) => {
+  return async () => {
+    try {
+      const { data } = await api.get(`almacen/inventario/control/bienes-consumo/get-list/?id_bodega=${filtros.seleccion_bodega === 'Todos' ? '' : filtros.seleccion_bodega}&id_bien=${filtros.seleccion_bien}&solicitable_vivero=${filtros.solicitable}&agrupado=${filtros.agrupar_bodega}`);
+      return data;
+    } catch (error: any) {
+      control_error(error.response.data.detail);
+      return error as AxiosError;
+    }
+  };
+};
+// Obtiene bienes fijos
 export const obtener_bienes: any = () => {
   return async () => {
     try {
       const { data } = await api.get(`almacen/inventario/control/activos-fijos/get-bienes/`);
+      return data;
+    } catch (error: any) {
+      control_error(error.response.data.detail);
+      return error as AxiosError;
+    }
+  };
+};
+// Obtiene bienes de consumo
+export const obtener_bienes_consumo: any = () => {
+  return async () => {
+    try {
+      const { data } = await api.get(`almacen/inventario/control/bienes-consumo/get-bienes/`);
       return data;
     } catch (error: any) {
       control_error(error.response.data.detail);
@@ -132,6 +156,18 @@ export const obtener_lista_origenes: any = () => {
   return async () => {
     try {
       const { data } = await api.get(`almacen/inventario/control/activos-fijos/get-list-origen/`);
+      return data;
+    } catch (error: any) {
+      control_error(error.response.data.detail);
+      return error as AxiosError;
+    }
+  };
+};
+// Obtiene listado de tipos de consumo
+export const obtener_lista_tipo: any = () => {
+  return async () => {
+    try {
+      const { data } = await api.get(`almacen/choices/tipo-elemento/`);
       return data;
     } catch (error: any) {
       control_error(error.response.data.detail);
