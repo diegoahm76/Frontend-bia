3fc69e8428ee1157c4f1dd2df0ec9c85d66ad074
John J Rodriguez <87795271+JohnRodriguezM@users.noreply.github.com>
2023-09-27 11:55:09 -0500

    ok

--- src/commons/gestorDocumental/controlAccesExped/components/parte4/components/BloqueAutorizacionCCD.tsx
+++ src/commons/gestorDocumental/controlAccesExped/components/parte4/components/BloqueAutorizacionCCD.tsx
@@ -6,7 +6,7 @@ import {
   FormControl,
   FormControlLabel,
   Grid,
-  Tooltip
+  Tooltip,
 } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 // import { Loader } from '../../../../../../../utils/Loader/Loader';
@@ -14,41 +14,79 @@ import AddIcon from '@mui/icons-material/Add';
 import InfoIcon from '@mui/icons-material/Info';
 import { RenderDataGrid } from '../../../../tca/Atom/RenderDataGrid/RenderDataGrid';
 import { useAppSelector } from '../../../../../../hooks';
+import { columnsBloqueAutorizacionCCD } from './columns/columnsBloqueAutorizacionCCD';
+import { EditIcon } from '@mui/icons-material/Edit';
 
 //! componente unidades organizacionales actuales de la sección responsable
 export const BloqueAutorizacionCCD: FC<any> = (): JSX.Element => {
+  const { controlAccesoExpedientesList } = useAppSelector(
+    (state) => state.ctrlAccesoExpSlice
+  );
 
-  const {controlAccesoExpedientesList} = useAppSelector((state) => state.ctrlAccesoExpSlice);
-
-/* const columns = [
-    ...columnsAsignacionPer,
+  const columns = [
+    ...columnsBloqueAutorizacionCCD,
     {
-      field: 'conceder_acceso_documentos_exps_no_propios',
-      headerName: 'Conceder acceso a docs',
-      width: 165,
+      headerName: 'Acciones',
+      field: 'accion',
       renderCell: (params: any) => (
-        <CheckboxComponent
-          checked={params.row.conceder_acceso_documentos_exps_no_propios}
-          handleChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-            handleCheckboxChangePRUEBA(
-              event,
-              'id_und_organizacional_actual',
-              params.row.id_und_organizacional_actual,
-              unidadActuales,
-              ['conceder_acceso_documentos_exps_no_propios', 'consultar_expedientes_no_propios'],
-              dispatch,
-              set_permisos_unidades_actuales_action
-            );
-          }}
-        />
+        <>
+          <IconButton
+            onClick={() => {
+              console.log(params?.row)
+             /* dispatch(get_trd_current(params.row));
+              closeModalModalSearchTRD();
+              dispatch(get_trds([]));
+              const ccd_current = {
+                id_ccd: params?.row?.id_ccd,
+                id_organigrama: params?.row?.id_organigrama
+              };
+              dispatch(
+                getServiceSeriesSubseriesXUnidadOrganizacional(ccd_current)
+              ).then((res: any) => {
+                dispatch(get_catalogo_trd(params.row.id_trd));
+              }); */
+            }}
+          >
+            <Avatar
+              sx={{
+                width: 24,
+                height: 24,
+                background: '#fff',
+                border: '2px solid'
+              }}
+              variant="rounded"
+            >
+              <EditIcon
+                titleAccess="Ver TRD"
+                sx={{ color: 'primary.main', width: '18px', height: '18px' }}
+              />
+            </Avatar>
+          </IconButton>
+        </>
       )
-    },
-  ]; */
+    }
+  ];
 
+  /*
+  "entidad_entera_consultar": false,
+  "entidad_entera_descargar": false,
+  "seccion_actual_respon_serie_doc_consultar": false,
+  "seccion_actual_respon_serie_doc_descargar": false,
+  "seccion_raiz_organi_actual_consultar": false,
+  "seccion_raiz_organi_actual_descargar": false,
+  "secciones_actuales_mismo_o_sup_nivel_respon_consulta": false,
+  "secciones_actuales_mismo_o_sup_nivel_respon_descargar": false,
+  "secciones_actuales_inf_nivel_respon_consultar": false,
+  "secciones_actuales_inf_nivel_respon_descargar": false,
+  "unds_org_sec_respon_mismo_o_sup_nivel_resp_exp_consultar": false,
+  "unds_org_sec_respon_mismo_o_sup_nivel_resp_exp_descargar": false,
+  "unds_org_sec_respon_inf_nivel_resp_exp_consultar": false,
+  "unds_org_sec_respon_inf_nivel_resp_exp_descargar": false,
+  */
 
-  if(controlAccesoExpedientesList?.length === 0) return <></>;
+  if (controlAccesoExpedientesList?.length === 0) return <></>;
 
-/*
+  /*
   if (loadingRestricciones)
     return (
       <Grid
@@ -70,7 +108,7 @@ export const BloqueAutorizacionCCD: FC<any> = (): JSX.Element => {
     <>
       <RenderDataGrid
         columns={[]}
-        rows={[]}
+        rows={controlAccesoExpedientesList ?? []}
         title="Listado bloque de autorización en el CCD"
       />
     </>
