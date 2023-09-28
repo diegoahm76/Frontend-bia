acbe411455c9e450f7d162b37c401c4b9ad90437
Palebaro <barpadroid@gmail.com>
2023-09-28 16:37:56 -0500

    Se realiza desarrollo parcial de entrega 77

--- src/commons/conservacion/Reportes/screens/analitica/AnaliticaSubsistema.tsx
+++ src/commons/conservacion/Reportes/screens/analitica/AnaliticaSubsistema.tsx
@@ -376,7 +376,7 @@ export const AnaliticaSubsistemaScreen: React.FC = () => {
                                                     {option[1]}
                                                 </li>
                                             )}
-                                            onChange={cambio_seleccion_tipo}
+                                            onChange={() =>{cambio_seleccion_tipo}}
                                             renderInput={(params) => (
                                                 <TextField {...params} label="Tipo de bien" />
                                             )}
@@ -402,7 +402,7 @@ export const AnaliticaSubsistemaScreen: React.FC = () => {
                                                     {option.nombre}
                                                 </li>
                                             )}
-                                            onChange={handle_change_autocomplete}
+                                            onChange={() => {handle_change_autocomplete}}
                                             renderInput={(params) => (
                                                 <TextField required {...params} label="Viveros" error={error_vivero} />
                                             )}
