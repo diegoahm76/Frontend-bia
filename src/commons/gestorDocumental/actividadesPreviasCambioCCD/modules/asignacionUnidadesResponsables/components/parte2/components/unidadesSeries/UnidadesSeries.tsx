61e3d8239b72cf0f92b4f1eaf11b8b425ed9a5e7
John J Rodriguez <87795271+JohnRodriguezM@users.noreply.github.com>
2023-10-10 15:24:58 -0500

    Ok

--- src/commons/gestorDocumental/actividadesPreviasCambioCCD/modules/asignacionUnidadesResponsables/components/parte2/components/unidadesSeries/UnidadesSeries.tsx
+++ src/commons/gestorDocumental/actividadesPreviasCambioCCD/modules/asignacionUnidadesResponsables/components/parte2/components/unidadesSeries/UnidadesSeries.tsx
@@ -5,14 +5,30 @@ import { containerStyles } from '../../../../../../../tca/screens/utils/constant
 import { RenderDataGrid } from '../../../../../../../tca/Atom/RenderDataGrid/RenderDataGrid';
 import { Title } from '../../../../../../../../../components';
 import { Subtitle } from '../../../../../../../../../components/Subtitle';
+import { Sub } from '../../../../../../../../../components/Sub';
 
 export const UnidadesSeries = (): JSX.Element => {
   return (
     <>
       <Grid container sx={containerStyles}>
         <Title title="Selección de secciones responsables del CCD nuevo sobre las series documentales de secciones del CCD actual" />
-        <Subtitle title="hello" />
+        <Title
+          title="Las asignaciones aquí realizadas son sólo para responsables sobre el catálogo de series, no continuarán con los permisos ni los consecutivos en el nuevo CCD"
+          width="98%"
+          fontSize="12.5px"
+        />
+        <Sub title="Éstas asignaciones se realizan cuando la sección o subsección actual no tienen persistencia en el nuevo CCD" width="80%" />
+
+
+
+
+        
         <RenderDataGrid
+          title="Éstas secciones continuarán con los consecutivos y permisos de su predecesor ( CCD ACTUAL / CCD NUEVO )"
+          columns={[]}
+          rows={[]}
+        />
+         <RenderDataGrid
           title="Éstas secciones continuarán con los consecutivos y permisos de su predecesor ( CCD ACTUAL / CCD NUEVO )"
           columns={[]}
           rows={[]}
--- src/components/Sub.tsx
+++ src/components/Sub.tsx
@@ -2,10 +2,9 @@ import { Box, Typography } from '@mui/material';
 interface Props {
   title: string;
   width?: string;
-  fontSize?: string;
 }
 // eslint-disable-next-line @typescript-eslint/naming-convention
-export const Title: React.FC<Props> = ({ title, width, fontSize }: Props) => {
+export const Sub: React.FC<Props> = ({ title, width }: Props) => {
   return (
     <Box
       className={`border px-4 text-white fs-5 p-1`}
@@ -13,22 +12,21 @@ export const Title: React.FC<Props> = ({ title, width, fontSize }: Props) => {
         display: 'grid',
         background:
           'transparent linear-gradient(269deg, #1492E6 0%, #062F48 34%, #365916 100%) 0% 0% no-repeat padding-box',
-        width: width || '100%',
         margin: '0 auto',
+        width: (width || '95%'),
         height: '40px',
         color: '#fff',
         borderRadius: '10px',
-        pl: '10px',
-        pr: '10px',
-        fontSize: fontSize || '17px',
-        fontWeight: '900',
+        pl: '20px',
+        mt: '10px',
         alignContent: 'center',
         marginTop: '10px',
       }}
     >
       <Typography
         sx={{
-          fontSize: fontSize || '17px',
+          fontSize: '.8rem',
+          fontWeight: '900',
         }}
       >
         {title}
