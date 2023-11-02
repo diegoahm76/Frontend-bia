685ec1f07347fdee2021a8b40e99d9fc28b03eb7
John J Rodriguez <87795271+JohnRodriguezM@users.noreply.github.com>
2023-11-02 11:59:19 -0500

    saved changes

--- src/commons/gestorDocumental/panelDeVentanilla/utils/extra/customTabPanel/CustomTabPanel.tsx
+++ src/commons/gestorDocumental/panelDeVentanilla/utils/extra/customTabPanel/CustomTabPanel.tsx
@@ -1,14 +1,13 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 
-import { Box, Typography } from "@mui/material";
-
+import { Box, Typography } from '@mui/material';
 
 interface TabPanelProps {
   children?: React.ReactNode;
   index: number;
   value: number;
 }
-export const CustomTabPanel  = (props: TabPanelProps) => {
+export const CustomTabPanel = (props: TabPanelProps) => {
   const { children, value, index, ...other } = props;
 
   return (
@@ -17,21 +16,24 @@ export const CustomTabPanel  = (props: TabPanelProps) => {
       hidden={value !== index}
       id={`simple-tabpanel-${index}`}
       aria-labelledby={`simple-tab-${index}`}
+      style={{}}
       {...other}
     >
       {value === index && (
+        <Box sx={{ p: 3 }}>{children}</Box>
+
+        /*
         <Box sx={{ p: 3 }}>
-          {/*revisar cuidadosamente porque el elemento no solo va a recibir un escrito simplemente*/}
           <Typography>{children}</Typography>
-        </Box>
+        </Box>*/
       )}
     </div>
   );
-}
+};
 
 export const a11yProps = (index: number) => {
   return {
     id: `simple-tab-${index}`,
     'aria-controls': `simple-tabpanel-${index}`,
   };
-}
+};
