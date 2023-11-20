1904f126261dfd8fd59da037984c06b643b8ee06
Palebaro <barpadroid@gmail.com>
2023-11-20 16:12:04 -0500

    Se ajusta busqueda de personas a nuevo filtro

--- src/commons/gestorDocumental/Expedientes/ConcesionAccesoExpedientes/screens/BusquedaPersona.tsx
+++ src/commons/gestorDocumental/Expedientes/ConcesionAccesoExpedientes/screens/BusquedaPersona.tsx
@@ -12,12 +12,17 @@ import { control_error } from '../../../../../helpers';
 import { CustomSelect, Title } from '../../../../../components';
 import { download_xls_dos } from '../../../../../documentos-descargar/XLS_descargar';
 import { download_pdf_dos } from '../../../../../documentos-descargar/PDF_descargar';
+import { useAppDispatch } from '../../../../../hooks';
+import { obtener_personas, obtener_tipo_documento } from '../thunks/ConcesionAccesoExpedientes';
+import { obtener_unidad_organizacional } from '../../aperturaExpedientes/thunks/aperturaExpedientes';
 
 interface IProps {
   set_persona: any;
 }
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const BusquedaPersona: React.FC<IProps> = (props: IProps) => {
+  const dispatch = useAppDispatch();
+
   const {
     register,
     handleSubmit: handle_submit,
@@ -27,73 +32,75 @@ export const BusquedaPersona: React.FC<IProps> = (props: IProps) => {
   const [tipo_documento_opt, set_tipo_documento_opt] = useState<IList[]>([]);
   const [tipo_documento_av, set_tipo_documento_av] = useState('');
   const [und_organizacional_av, set_und_organizacional_av] = useState('');
+  const [lt_unidades_org, set_lt_unidades_org] = useState<IList[]>([]);
   const [rows, set_rows] = useState<InfoPersona[]>([]);
   const [is_search, set_is_search] = useState(false);
   const [is_loading, set_is_loading] = useState(false);
 
   useEffect(() => {
-    void get_selects_options();
+    obtener_tipo_documento_fc();
+    obtener_unidad_organizacional_fc();
   }, []);
 
-  const get_selects_options = async (): Promise<void> => {
-    set_is_loading(true);
-    try {
-      const {
-        data: { data: res_tipo_documento },
-      } = await get_tipo_documento();
-      set_tipo_documento_opt(res_tipo_documento ?? []);
-    } catch (err) {
-      control_error(err);
-    } finally {
-      set_is_loading(false);
-    }
+  const obtener_tipo_documento_fc = async (): Promise<void> => {
+    dispatch(obtener_tipo_documento()).then((response: any) => {
+      if (response.success) {
+        const personas_naturales_opc = response.data.filter((rd: any) => rd.value !== 'NT');
+        set_tipo_documento_opt(personas_naturales_opc);
+      }
+    })
+  };
+  const obtener_unidad_organizacional_fc = async (): Promise<void> => {
+    dispatch(obtener_unidad_organizacional()).then((response: any) => {
+      if (response.success) {
+        const org_activas = response.data.filter((rd: any) => rd.activo);
+        const list_opc = org_activas.map((oa: any) => {
+          return { label: oa.nombre, value: oa.id_unidad_organizacional }
+        });
+        set_lt_unidades_org(list_opc);
+      }
+    })
   };
 
   const on_submit_advance = handle_submit(
     async ({
-      und_organizacional,
       tipo_documento,
       numero_documento,
       primer_nombre,
+      segundo_nombre,
       primer_apellido,
-      razon_social,
-      nombre_comercial,
+      segundo_apellido,
+      und_organizacional
     }) => {
       set_is_search(true);
-      try {
-        set_rows([]);
-        const {
-          data: { data },
-        } = await search_avanzada({
-          // und_organizacional,
-          tipo_documento,
-          numero_documento,
-          primer_nombre,
-          primer_apellido,
-          razon_social,
-          nombre_comercial,
-        });
-
-        if (data?.length > 0) {
-          set_rows(data);
+      set_rows([]);
+      dispatch(obtener_personas(
+        tipo_documento,
+        numero_documento,
+        primer_nombre,
+        segundo_nombre,
+        primer_apellido,
+        segundo_apellido,
+        und_organizacional
+      )).then((response: any) => {
+        if (response.success) {
+          set_rows(response.data);
+          set_is_search(false);
         }
-      } catch (error) {
-        const temp_error = error as AxiosError;
-        const resp = temp_error.response?.data as ResponseServer<any>;
-        control_error(resp.detail);
-      } finally {
-        set_is_search(false);
-      }
+      })
     }
   );
 
-  const handle_change_select = (e: SelectChangeEvent<string>): void => {
+  const cambio_tipo_doc = (e: SelectChangeEvent<string>): void => {
     set_tipo_documento_av(e.target.value);
+  };
+  const cambio_und_org = (e: SelectChangeEvent<string>): void => {
+    set_und_organizacional_av(e.target.value);
   };
 
   const columnsss = [
     {
-      field: "tipo_persona",
+      field: "tipo_persona_desc",
       header: "Tipo persona",
       style: { minWidth: '150px' },
     },
@@ -128,15 +135,10 @@ export const BusquedaPersona: React.FC<IProps> = (props: IProps) => {
       style: { minWidth: '180px' },
     },
     {
-      field: "razon_social",
-      header: "Razón social",
-      style: { minWidth: '180px' },
-    },
-    {
-      field: "nombre_comercial",
-      header: "Nombre comercial",
-      style: { minWidth: '180px' },
-    },
+      field: "nombre_unidad_organizacional_actual",
+      header: "Unidad organizacional",
+      style: { minWidth: '200px' },
+    }
   ];
 
   return (
@@ -159,22 +161,9 @@ export const BusquedaPersona: React.FC<IProps> = (props: IProps) => {
             boxShadow: '0px 3px 6px #042F4A26',
           }}
         >
-          {/* <Grid item xs={12} sm={6} md={12}>
+          <Grid item xs={12} sm={6} md={4}>
             <CustomSelect
-              onChange={handle_change_select}
-              label="Unidad organizacional *"
-              name="und_organizacional"
-              value={und_organizacional_av}
-              options={tipo_documento_opt}
-              disabled={is_loading}
-              required={true}
-              errors={errors}
-              register={register}
-            />
-          </Grid> */}
-          <Grid item xs={12} sm={6} md={3}>
-            <CustomSelect
-              onChange={handle_change_select}
+              onChange={cambio_tipo_doc}
               label="Tipo de documento *"
               name="tipo_documento"
               value={tipo_documento_av}
@@ -185,7 +174,7 @@ export const BusquedaPersona: React.FC<IProps> = (props: IProps) => {
               register={register}
             />
           </Grid>
-          <Grid item xs={12} sm={6} md={3}>
+          <Grid item xs={12} sm={6} md={4}>
             <TextField
               fullWidth
               label="Número de documento *"
@@ -204,46 +193,52 @@ export const BusquedaPersona: React.FC<IProps> = (props: IProps) => {
               })}
             />
           </Grid>
-          {tipo_documento_av !== 'NT' ? (
-            <>
-              <Grid item xs={12} sm={6} md={2}>
-                <TextField
-                  fullWidth
-                  label="Primer nombre"
-                  size="small"
-                  {...register('primer_nombre')}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6} md={2}>
-                <TextField
-                  fullWidth
-                  label="Primer apellido"
-                  size="small"
-                  {...register('primer_apellido')}
-                />
-              </Grid>
-            </>
-          ) : (
-            <>
-              <Grid item xs={12} sm={6} md={2}>
-                <TextField
-                  fullWidth
-                  label="Razón social"
-                  size="small"
-                  {...register('razon_social')}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6} md={2}>
-                <TextField
-                  fullWidth
-                  label="Nombre comercial"
-                  size="small"
-                  {...register('nombre_comercial')}
-                />
-              </Grid>
-            </>
-          )}
-          <Grid item xs={12} sm={6} md={2} container justifyContent="end">
+          <Grid item xs={12} sm={6} md={4}>
+            <CustomSelect
+              onChange={cambio_und_org}
+              label="Unidad organizacional *"
+              name="und_organizacional"
+              value={und_organizacional_av}
+              options={lt_unidades_org}
+              disabled={is_loading}
+              // required={true}
+              errors={errors}
+              register={register}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} md={2}>
+            <TextField
+              fullWidth
+              label="Primer nombre"
+              size="small"
+              {...register('primer_nombre')}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} md={2}>
+            <TextField
+              fullWidth
+              label="Segundo nombre"
+              size="small"
+              {...register('segundo_nombre')}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} md={2}>
+            <TextField
+              fullWidth
+              label="Primer apellido"
+              size="small"
+              {...register('primer_apellido')}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} md={2}>
+            <TextField
+              fullWidth
+              label="Segundo apellido"
+              size="small"
+              {...register('segundo_apellido')}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} md={4} container justifyContent="end">
             <LoadingButton
               type="submit"
               variant="contained"
---
+++ src/commons/gestorDocumental/Expedientes/ConcesionAccesoExpedientes/thunks/ConcesionAccesoExpedientes.tsx
@@ -0,0 +1,86 @@
+
+import { api } from '../../../../../api/axios';
+// Types
+import { type AxiosError, } from 'axios';
+// Reducers
+import { toast, type ToastContent } from 'react-toastify';
+
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const control_error = (message: ToastContent = 'Algo pasó, intente de nuevo') =>
+  toast.error(message, {
+    position: 'bottom-right',
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: 'light'
+  });
+
+  const control_success = (message: ToastContent) =>
+  toast.success(message, {
+    position: 'bottom-right',
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: 'light'
+  });
+
+// Lista Tipo Documento
+export const obtener_tipo_documento: any = () => {
+  return async () => {
+    try {
+      const { data } = await api.get(`listas/tipo-documento/`);
+      return data;
+    } catch (error: any) {
+      control_error(error.response.data.detail);
+      return error as AxiosError;
+    }
+  };
+};
+// Buscar personas por filtros
+export const obtener_personas: any = (tipo_documento: string,numero_documento: string,primer_nombre: string,segundo_nombre: string,primer_apellido: string,segundo_apellido: string,id_unidad_organizacional_actual: number) => {
+  return async () => {
+    try {
+      const { data } = await api.get(`gestor/expedientes-archivos/expedientes/concesion-acceso/personas/get-by-filters/?tipo_documento=${tipo_documento}&numero_documento=${numero_documento}&primer_nombre=${primer_nombre ?? ''}&segundo_nombre=${segundo_nombre ?? ''}&primer_apellido=${primer_apellido ?? ''}&segundo_apellido=${segundo_apellido ?? ''}&id_unidad_organizacional_actual=${id_unidad_organizacional_actual ?? ''}`);
+      return data;
+    } catch (error: any) {
+      control_error(error.response.data.detail);
+      return error as AxiosError;
+    }
+  };
+};
+
+// Actualizar expediente
+export const actualizar_expediente: any = (id_expediente: number,expediente: any) => {
+  return async () => {
+    try {
+      const { data } = await api.put(`gestor/expedientes-archivos/expedientes/apertura-expediente/update/${id_expediente}/`,expediente);
+      control_success('El expediente fue actualizado correctamente.');
+      return data;
+    } catch (error: any) {
+      control_error(error.response.data.detail);
+      return error as AxiosError;
+    }
+  };
+};
+
+// Anulación expedientes
+export const anular_expediente: any = (id_expediente: number,motivo: any) => {
+  return async () => {
+    try {
+      const { data } = await api.put(`gestor/expedientes-archivos/expedientes/apertura-expediente/anular/${id_expediente}/`,motivo);
+      control_success('El expediente se anuló correctamente.');
+      return data;
+    } catch (error: any) {
+      control_error(error.response.data.detail);
+      return error as AxiosError;
+    }
+  };
+};
+
