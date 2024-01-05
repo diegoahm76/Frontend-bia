25bb4e74e03704443e59d9e5c21fd0e1c6453663
Guillermo Sarmiento <guillermo.sarmiento@macarenia.org>
2024-01-05 16:08:43 -0500

    Features: ExpedientesRouter, Production feature

--- src/index.tsx
+++ src/index.tsx
@@ -14,6 +14,7 @@ import 'primeicons/primeicons.css';
 import { Loader } from './utils/Loader/Loader';
 import setupLocatorUI from '@locator/runtime';
 
+//En productivo comentar la siguiente linea
 if (process.env.NODE_ENV === 'development') {
   setupLocatorUI();
 }
