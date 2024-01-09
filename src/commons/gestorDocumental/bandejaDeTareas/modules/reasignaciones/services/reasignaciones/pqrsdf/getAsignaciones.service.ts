3c2d02d453202474aafb5cdb26240baf3ebf16bc
John J Rodriguez <87795271+JohnRodriguezM@users.noreply.github.com>
2024-01-08 21:42:39 -0500

    feature / initial commit reasignacion de tarea

---
+++ src/commons/gestorDocumental/bandejaDeTareas/modules/reasignaciones/services/reasignaciones/pqrsdf/getAsignaciones.service.ts
@@ -0,0 +1,33 @@
+import { api } from '../../../../../../../../api/axios';
+import { control_success } from '../../../../../../../../helpers';
+import { showAlert } from '../../../../../../../../utils/showAlert/ShowAlert';
+import { control_warning } from '../../../../../../../almacen/configuracion/store/thunks/BodegaThunks';
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export const getAsignaciones = async (
+  id: string,
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>
+) => {
+  try {
+    setLoading(true);
+    const url = `gestor/panel_ventanilla/asignar-pqrsdf/get/${id}/`;
+    const { data } = await api.get(url);
+
+    if (data?.data?.length === 0) {
+      control_warning('No se encontraron asignaciones');
+      return [];
+    } else {
+      control_success('Asignaciones obtenidas');
+      return data?.data;
+    }
+  } catch (error) {
+    showAlert(
+      'Atención',
+      'Sin asignaciones realizadas para este elemento',
+      'info'
+    )
+    return [];
+  } finally {
+    setLoading(false);
+  }
+};
