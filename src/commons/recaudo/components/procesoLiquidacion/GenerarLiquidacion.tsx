23b3a24cfe3c57020a95ed1367d5724bfca073ca
miguel-73 <miguelangelmurciaocampo73@gmail.com>
2024-02-06 11:14:21 -0500

    ajuste decimales liquidacion

--- src/commons/recaudo/components/procesoLiquidacion/GenerarLiquidacion.tsx
+++ src/commons/recaudo/components/procesoLiquidacion/GenerarLiquidacion.tsx
@@ -5809,7 +5809,7 @@ export const GenerarLiquidacion: React.FC<IProps> = ({
         }}
       >
         <Typography color='black' variant="h4">Total de la obligacion</Typography>
-        <Typography color='green' variant="h4" sx={{ textAlign: 'center' }}>{currency_formatter(form_liquidacion.valor ?? 0, 4)}</Typography>
+        <Typography color='green' variant="h4" sx={{ textAlign: 'center' }}>{currency_formatter(form_liquidacion.valor ?? 0, 0)}</Typography>
       </Grid>
 
       <Grid container justifyContent={'center'} spacing={3}>
