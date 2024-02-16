d63fb09c7108cc230dcb189e9b018e60e900eab8
John J Rodriguez <87795271+JohnRodriguezM@users.noreply.github.com>
2024-02-16 10:07:23 -0500

    release / consulta estado de opas 100%

--- src/commons/gestorDocumental/consulltaEstadoSolicitudes/services/otros/consultaInternoOtros.service.ts
+++ src/commons/gestorDocumental/consulltaEstadoSolicitudes/services/otros/consultaInternoOtros.service.ts
@@ -14,13 +14,16 @@ const cargarAsignacionesOtros = async (
     const response = await api.get(
       `/gestor/radicados/otros/consulta-estado-otros/?radicado=${formData.radicado}&fecha_radicado_desde=${formData.fecha_inicio}&fecha_radicado_hasta=${formData.fecha_fin}&estado_solicitud=${formData.estado}`
     );
-    if (response.data.success) {
+    if (response.data.success && response?.data?.data && response?.data?.data?.length > 0) {
       setAsignaciones(response?.data?.data);
       console.log('soy la data', response?.data?.data);
       control_success(
         'Se ha encontrado los siguientes datos que coinciden con la busqueda'
       );
+      return;
     }
+    showAlert('Opss', 'No se recibieron datos de la solicitud o la respuesta está vacía', 'warning');
+    return []
   } catch (error: any) {
     console.error('Error al cargar las asignaciones de encuesta', error);
     // control_error(error.response.data.detail);
