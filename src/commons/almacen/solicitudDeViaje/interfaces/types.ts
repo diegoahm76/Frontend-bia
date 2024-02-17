fce58f31a18e5a0a6088c6e4d017695e6ff4511e
rickbroken <daironquebrada@gmail.com>
2024-02-17 08:07:01 -0500

    feature / Se agrega interface para la solicitud de viajes

--- src/commons/almacen/solicitudDeViaje/interfaces/types.ts
+++ src/commons/almacen/solicitudDeViaje/interfaces/types.ts
@@ -10,3 +10,20 @@ export interface data_solicitud_viaje {
   fechaRetorno: string;
   municipioDestino: string;
 }
+
+export interface interface_solicitar_viaje {
+  motivo_viaje_solicitado: string,  // Motivo del viaje
+  cod_municipio: string,  // Código del municipio de destino
+  cod_departamento: string,  // Código del departamento de destino
+  tiene_expediente_asociado: boolean,  // Indica si tiene un expediente asociado
+  id_expediente_asociado: number,  // ID del expediente asociado, si corresponde
+  direccion: string,  // Dirección del destino
+  nro_pasajeros: number,  // Número de pasajeros
+  fecha_partida: string,  // Fecha de partida
+  hora_partida: string,  // Hora de partida
+  fecha_retorno: string,  // Fecha de retorno
+  hora_retorno: string,  // Hora de retorno
+  req_compagnia_militar: boolean,  // Indica si se requiere compañía militar
+  consideraciones_adicionales: string,  // Consideraciones adicionales
+  indicaciones_destino: string  // Indicaciones para llegar al destino
+}
