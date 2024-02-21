e6065e59454ebfcedfe76d8e71c9c133dd01073a
rickbroken <daironquebrada@gmail.com>
2024-02-21 13:11:31 -0500

    feature / se agregan interfaces y se modifican algunos valores de interfaces ya creadas

--- src/commons/almacen/solicitudDeViaje/interfaces/types.ts
+++ src/commons/almacen/solicitudDeViaje/interfaces/types.ts
@@ -6,7 +6,8 @@ export interface data_solicitud_viaje {
   id_solicitud_viaje: number
   fecha_solicitud: string
   tiene_expediente_asociado: boolean
-  motivo_viaje: string
+  motivo_viaje?: string
+  motivo_viaje_solicitado?: string
   direccion: string
   indicaciones_destino: string
   nro_pasajeros: number
@@ -23,11 +24,12 @@ export interface data_solicitud_viaje {
   estado_solicitud: string
   id_persona_solicita: number
   id_unidad_org_solicita: number
-  id_expediente_asociado: number
+  id_expediente_asociado?: number
   cod_municipio: string
   id_persona_responsable: any
   id_unidad_org_responsable: any
-  eliminar?: number
+  id_solicitud?: number
+  cod_departamento: string
 }
 
 export interface data_row_solicitud_viaje {
@@ -37,13 +39,14 @@ export interface data_row_solicitud_viaje {
   fecha_retorno: string
   estado_solicitud: string
   cod_municipio: string
-  eliminar: number
+  id_solicitud?: number
 }
 
 export interface interface_solicitar_viaje {
-  motivo_viaje_solicitado: string,  // Motivo del viaje
-  cod_municipio: string,  // Código del municipio de destino
-  cod_departamento: string,  // Código del departamento de destino
+  motivo_viaje_solicitado?: string,  // Motivo del viaje
+  motivo_viaje?: string,  // Motivo del viaje
+  cod_municipio: string, // Código del municipio de destino
+  cod_departamento?: string,  // Código del departamento de destino
   tiene_expediente_asociado: boolean,  // Indica si tiene un expediente asociado
   id_expediente_asociado?: number,  // ID del expediente asociado, si corresponde
   direccion: string,  // Dirección del destino
@@ -53,6 +56,7 @@ export interface interface_solicitar_viaje {
   fecha_retorno: string,  // Fecha de retorno
   hora_retorno: string,  // Hora de retorno
   req_compagnia_militar: boolean,  // Indica si se requiere compañía militar
+  requiere_carga: boolean,  // Indica si se requiere compañía militar
   consideraciones_adicionales: string,  // Consideraciones adicionales
   indicaciones_destino: string  // Indicaciones para llegar al destino
 }
