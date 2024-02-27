f900ec35e52eba6be08b707d382cb6f159955601
John J Rodriguez <87795271+JohnRodriguezM@users.noreply.github.com>
2024-02-27 05:47:17 -0500

    feature/ reasignacion a grupo tramite

--- src/commons/gestorDocumental/bandejaDeTareas/modules/reasignaciones/services/post/tramitesServicios/postReasTram.service.ts
+++ src/commons/gestorDocumental/bandejaDeTareas/modules/reasignaciones/services/post/tramitesServicios/postReasTram.service.ts
@@ -3,20 +3,14 @@ import { api } from '../../../../../../../../api/axios';
 import { control_error, control_success } from '../../../../../../../../helpers';
 import { showAlert } from '../../../../../../../../utils/showAlert/ShowAlert';
 
-/*{
-
-  "id_pqrsdf":27,
-  "id_persona_asignada":218,
-  "id_und_org_seccion_asignada":5248
-}*/
-export const postReAsignacionTarea = async (
+export const postReAsignacionTareaTramite = async (
   data: any,
   setLoading: React.Dispatch<React.SetStateAction<boolean>>
 ) => {
   try {
     console.log(data);
    setLoading(true);
-    const url = `gestor/bandeja-tareas/reasignaciones/tareas/create/`;
+    const url = `gestor/bandeja-tareas/reasignaciones/tramites/tareas/create/`;
     const response = await api.post(url, data);
     control_success('Se realizó la re-asignación correctamente');
     return response;
