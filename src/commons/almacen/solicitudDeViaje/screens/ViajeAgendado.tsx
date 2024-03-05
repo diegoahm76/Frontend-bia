9ec726a17122b97da23067d883580c39cd13eccc
rickbroken <daironquebrada@gmail.com>
2024-03-05 18:36:54 -0500

    feature / se agrega data para componente de Viaje agendado, para la funcion de ver solicitud de viaje

--- src/commons/almacen/solicitudDeViaje/screens/ViajeAgendado.tsx
+++ src/commons/almacen/solicitudDeViaje/screens/ViajeAgendado.tsx
@@ -4,23 +4,24 @@ import { Title } from '../../../../components';
 import { DatePicker, LocalizationProvider, MobileTimePicker } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import dayjs, { Dayjs } from 'dayjs';
-import { viajes_agendados } from '../interfaces/types';
 import { parseHora } from '../thunks/viajes';
+import { interface_solicitud_respondida } from '../interfaces/types';
 
 interface props {
-  solicitud_respondida: viajes_agendados;
+  solicitud_respondida: interface_solicitud_respondida;
 }
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 const ViajeAgendado: React.FC<props> = ({solicitud_respondida}) => {
-  const [fecha_autorizacion, set_fecha_autorizacion] = useState<Dayjs>(dayjs());
-  const [fecha_salida, set_fecha_salida] = useState<Dayjs>(dayjs());
-  const [fecha_retorno, set_fecha_retorno] = useState<Dayjs>(dayjs());
+  const [fecha_autorizacion, set_fecha_autorizacion] = useState<Dayjs | null>(dayjs());
+  const [fecha_salida, set_fecha_salida] = useState<Dayjs | null>(dayjs());
+  const [fecha_retorno, set_fecha_retorno] = useState<Dayjs | null>(dayjs());
   const [hora_salida, set_hora_salida] = useState<Date | null>(new Date());
   const [hora_retorno, set_hora_retorno] = useState<Date | null>(new Date());
   const [nombres_conductor, set_nombres_conductor] = useState<string>('');
   const [apellidos_conductor, set_apellidos_conductor] = useState<string>('');
   const [nombre_vehiculo, set_nombre_vehiculo] = useState<string>('');
+  const [documento_conductor, set_documento_conductor] = useState<string>('');
   const [marca_vehiculo, set_marca_vehiculo] = useState<string>('');
   const [placa_vehiculo, set_placa_vehiculo] = useState<string>('');
 
@@ -28,18 +29,25 @@ const ViajeAgendado: React.FC<props> = ({solicitud_respondida}) => {
   const cambio_fecha_autorizacion = (date: Dayjs | null): void => {
     if (date !== null) {
       set_fecha_autorizacion(date);
+    } else {
+      set_fecha_autorizacion(dayjs());
     }
   };
 
   const cambio_fecha_salida = (date: Dayjs | null): void => {
+    console.log(date);
     if (date !== null) {
       set_fecha_salida(date);
+    } else {
+      set_fecha_salida(dayjs());
     }
   };
 
   const cambio_fecha_retorno = (date: Dayjs | null): void => {
     if (date !== null) {
       set_fecha_retorno(date);
+    } else {
+      set_fecha_retorno(dayjs());
     }
   };
 
@@ -54,16 +62,17 @@ const ViajeAgendado: React.FC<props> = ({solicitud_respondida}) => {
   
   useEffect(()=>{
     if(Object.keys(solicitud_respondida).length !== 0){
-      cambio_fecha_salida(dayjs(solicitud_respondida.fecha_partida_asignada));
-      cambio_fecha_retorno(dayjs(solicitud_respondida.fecha_retorno_asignada));
+      cambio_fecha_salida(dayjs(solicitud_respondida.fecha_partida_asignada ?? '2024-01-01'));
+      cambio_fecha_retorno(dayjs(solicitud_respondida.fecha_retorno_asignada ?? '2024-01-01'));
       cambio_hora_salida(parseHora(solicitud_respondida.hora_partida));
       cambio_hora_retorno(parseHora(solicitud_respondida.hora_retorno));
       set_nombres_conductor(solicitud_respondida.nombre_conductor);
       set_apellidos_conductor(solicitud_respondida.apellido_conductor);
+      set_documento_conductor(solicitud_respondida.numero_documento);
       set_nombre_vehiculo(solicitud_respondida.nombre);
       set_marca_vehiculo(solicitud_respondida.marca);
       set_placa_vehiculo(solicitud_respondida.placa);
-      cambio_fecha_autorizacion(dayjs(solicitud_respondida.fecha_autorizacion));
+      cambio_fecha_autorizacion(dayjs(solicitud_respondida.fecha_autorizacion ?? '2024-01-01'));
     }
   },[solicitud_respondida])
 
@@ -105,6 +114,17 @@ const ViajeAgendado: React.FC<props> = ({solicitud_respondida}) => {
 
       <Grid item xs={12} lg={2}>
         <TextField
+          label="Documento conductor:"
+          fullWidth 
+          size="small"
+          disabled
+          value={documento_conductor}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>)=>set_documento_conductor(e.target.value)}
+        />
+      </Grid>
+
+      <Grid item xs={12} lg={2}>
+        <TextField
           label="Nombre vehículo:"
           fullWidth 
           size="small"
@@ -136,26 +156,7 @@ const ViajeAgendado: React.FC<props> = ({solicitud_respondida}) => {
         />
       </Grid>
 
-      <Grid item xs={12} lg={2}>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DatePicker
-            disabled
-            label="Fecha de autorizacion:"
-            value={fecha_autorizacion}
-            onChange={(newValue) => { cambio_fecha_autorizacion(newValue); }}
-            renderInput={(params) => (
-              <TextField
-                required
-                fullWidth
-                size="small"
-                {...params}
-              />
-            )}
-          />
-        </LocalizationProvider>
-      </Grid>
-
-      <Grid item xs={12} lg={4.5}>
+      <Grid item xs={12} lg={3}>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <DatePicker
             disabled
@@ -189,7 +190,7 @@ const ViajeAgendado: React.FC<props> = ({solicitud_respondida}) => {
         </LocalizationProvider>
       </Grid>
 
-      <Grid item xs={12} lg={4.5}>
+      <Grid item xs={12} lg={3}>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <DatePicker
             disabled
@@ -220,6 +221,25 @@ const ViajeAgendado: React.FC<props> = ({solicitud_respondida}) => {
                 <TextField {...params} fullWidth variant="standard" helperText="" />
               )}
             />
+        </LocalizationProvider>
+      </Grid>
+
+      <Grid item xs={12} lg={3}>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePicker
+            disabled
+            label="Fecha de autorizacion:"
+            value={fecha_autorizacion}
+            onChange={(newValue) => { cambio_fecha_autorizacion(newValue); }}
+            renderInput={(params) => (
+              <TextField
+                required
+                fullWidth
+                size="small"
+                {...params}
+              />
+            )}
+          />
         </LocalizationProvider>
       </Grid>
 
