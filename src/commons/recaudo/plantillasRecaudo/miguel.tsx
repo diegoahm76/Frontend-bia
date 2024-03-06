c55212a5e615af1db8ef3c0bb225ae3899b32366
miguel-73 <miguelangelmurciaocampo73@gmail.com>
2024-03-06 14:32:10 -0500

    ajsute liquidacion, ajuste consulta de solicitud ,

---
+++ src/commons/recaudo/plantillasRecaudo/miguel.tsx
@@ -0,0 +1,73 @@
+ 
+    
+    function remicion_viso(email: any) {
+        return`  
+                                             Expediente No  {{{3.37.1.013.001}}}
+        
+        En aplicación del artículo 69 CPACA, comedidamente remitimos NOTIFICACIÓN POR AVISO del Auto (X) o Resolución ( ) {{{{ Nº PS-GJ. 1.2.64.23.3165}}}} de fecha {{{ 27/09/2023}}}, expedido por SUBDIRECCIÓN DE AUTORIDAD AMBIENTAL, anexando a la presente copia informal de un ejemplar de la actuación administrativa, dejando expresa constancia que contra él {{{{SI (  ) o NO (X)}}}} procede recurso de reposición dentro de los diez   (--) días siguientes a la fecha de surtida la presente notificación.
+        
+        La presente notificación se considerará surtida al finalizar el día siguiente al de la entrega del aviso en el lugar de destino. 
+        
+        Se informa al usuario que cualquier solicitud, petición o descargos, puede ser enviada al correo de la Corporación: info@cormacarena.gov.co.
+        
+    
+        
+         
+        
+    `};
+
+    function  constancia_publicacion (email: any) {
+        return`
+
+        En Villavicencio, el día  04  del mes  AGOSTO del año  2023  se procede a realizar citación al señor(a) (X) empresa (  )  JOSÉ ROBERT TRIANA SÁNCHEZ, identificado (a) con cédula de ciudadanía (X) o NIT ( ) No.  1.123.140.854, para que en término de cinco (5) días siguientes a la fijación de la presente, comparezca a la Corporación para el Desarrollo Sostenible del Área Especial de la Macarena “CORMACARENA” para diligencia de Notificación personal de:
+
+        Auto (X) o Resolución ( ) 
+        No. PS-GJ. 1.2.64.23.2534 
+        Fecha del acto administrativo  14/07/2023 
+        Expediente No. 3.47.2.016.037  
+        Emitido (a) por SUBDIRECCION DE AUTORIDAD AMBIENTAL
+        
+        CONSTANCIA DE FIJACIÓN. Siendo las 08:00 (X) AM (  ) PM del día  04  del mes AGOSTO del año 2023, se fija la presente  citación en la página web de la Corporación o en un lugar de acceso al público.
+          
+        CONSTANCIA DE DESFIJACIÓN: Siendo las 06:00 (  ) AM (X) PM del día  11  del mes AGOSTO del año 2023, se desfija la presente citación, después de haber permanecido por el término de cinco (05) días legales.
+        
+        Transcurrido dicho término sin realizarse notificación personal de la actuación, se procederá a la Notificación por aviso, en los términos del artículo 69 del CPACA. 
+        `};
+
+        function  constancia_publicacion2 (email: any) {
+            return`
+    
+            En Villavicencio, el día 26 del mes  OCTUBRE del año 2023 se procede a realizar la Notificación por Aviso al señor(a) (X) empresa ( )  GERMÁN CELY PALACIOS,  identificado (a) con cédula de ciudadanía (X) o NIT  ( ) No.  17.334.194,  de -------------
+
+
+            Auto ( ) o Resolución (X) 
+            No. PS-GJ. 1.2.6.23.0881 
+            Fecha del acto administrativo  14/06/2023 
+            Expediente No 3.11.022.643   
+            Emitido (a) por SUBDIRECCIÓN DE AUTORIDAD AMBIENTAL.
+            
+            Dejando  expresa  constancia  que  contra él SI ( ) o NO (X) procede recurso de reposición, dentro de los (X) días siguientes a la fecha de surtida la presente notificación, el cual lo podrá interponer ante ----.
+            
+            CONSTANCIA DE FIJACIÓN. Siendo las 08:00 (X) AM (  ) PM del día 26 del mes OCTUBRE del año 2023, se fija el presente  aviso en la página web de la Corporación y/o en un lugar de acceso al público.
+            
+            CONSTANCIA DE DESFIJACIÓN: Siendo las 08:00 (X) AM () PM del día 01 del mes NOVIEMBRE del año 2023, se desfija el presente  aviso, después de haber permanecido por el término de cinco (05) días legales.
+                
+            Se advierte que la notificación se entiende surtida al finalizar el día siguiente de la desfijación del presente Aviso, es decir, se encuentra NOTIFICADO el día  02  del mes NOVIEMBRE del año 2023 conforme a lo establecido en el artículo 69 de la Ley 1437 de 2011.
+       
+            `};
+
+
+
+
+
+
+
+            
+    export { 
+        remicion_viso, 
+        constancia_publicacion,
+        constancia_publicacion2
+     };
+
+
+ 
