ec1350d16d894862b827ed04e1d70c110019d5ba
JohnRodriguezM <johnjairorodriguez384@gmail.com>
2024-03-17 20:47:19 -0500

    feature/ opa

--- src/commons/gestorDocumental/TramitesOServicios/respuestaRequerimientoOpa/router/ResReqOpaRouter.tsx
+++ src/commons/gestorDocumental/TramitesOServicios/respuestaRequerimientoOpa/router/ResReqOpaRouter.tsx
@@ -1,13 +1,13 @@
 import type { ReactElement } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { ResReqOpaMain } from '../screen/ResReqOpaMain';
+import { MainFirstPartResReqOpa } from '../modules/firstPart/screen/MainFirstPartResReqOpa';
 
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const ResReqOpaRouter = (): ReactElement => {
   return (
     <Routes>
-      <Route path="/" element={<ResReqOpaMain/>} />
+      <Route path="/" element={<MainFirstPartResReqOpa/>} />
       <Route path="formulario_usuario" element={<>Aquí se desarrollara los formularios de la respuesta del requerimiento de la opa</>} />
     </Routes>
   );
--- src/commons/gestorDocumental/TramitesOServicios/respuestaRequerimientoOpa/screen/ResReqOpaMain.tsx
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-
-export const ResReqOpaMain = (): JSX.Element => {
-  return (
-    <div>ResReqOpaMain</div>
-  )
-}
