4fab1e343a9e61634bc3c31668bd337e2a4eb1f9
JohnRodriguezM <johnjairorodriguez384@gmail.com>
2024-03-17 22:06:39 -0500

    fix /structure context

--- src/commons/gestorDocumental/TramitesOServicios/respuestaRequerimientoOpa/router/ResReqOpaRouter.tsx
+++ src/commons/gestorDocumental/TramitesOServicios/respuestaRequerimientoOpa/router/ResReqOpaRouter.tsx
@@ -3,15 +3,18 @@ import { Route, Routes } from 'react-router-dom';
 import { MainFirstPartResReqOpa } from '../modules/firstPart/screen/MainFirstPartResReqOpa';
 import { MainResReqOpaScreen } from '../modules/secondPart/screen/MainResReqOpaScreen';
 import { ResRequerimientoOpaProvider } from '../modules/secondPart/context/ResRequerimientoOpaContext';
+import { BandejaTareasProvider } from '../../../bandejaDeTareas/mainModule/context/BandejaTareasContext';
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const ResReqOpaRouter = (): ReactElement => {
   return (
-    <ResRequerimientoOpaProvider>
-      <Routes>
-        <Route path="/" element={<MainFirstPartResReqOpa />} />
-        <Route path="formulario_usuario" element={<MainResReqOpaScreen />} />
-      </Routes>
-    </ResRequerimientoOpaProvider>
+    <BandejaTareasProvider>
+      <ResRequerimientoOpaProvider>
+        <Routes>
+          <Route path="/" element={<MainFirstPartResReqOpa />} />
+          <Route path="formulario_usuario" element={<MainResReqOpaScreen />} />
+        </Routes>
+      </ResRequerimientoOpaProvider>
+    </BandejaTareasProvider>
   );
 };
