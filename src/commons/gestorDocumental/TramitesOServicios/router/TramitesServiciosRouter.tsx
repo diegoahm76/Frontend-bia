20bb6a1408f036b353d46a977857ae975f4f6968
HectorStiven <vigoyahecto90@gmail.com>
2024-03-26 04:23:13 -0500

    modal de metadatos ajustado a un 95 %

--- src/commons/gestorDocumental/TramitesOServicios/router/TramitesServiciosRouter.tsx
+++ src/commons/gestorDocumental/TramitesOServicios/router/TramitesServiciosRouter.tsx
@@ -3,18 +3,21 @@ import { Route, Routes } from 'react-router-dom';
 import { Page404 } from '../../../../screens/404';
 import { UserProvider } from '../../deposito/Estantes/context/context';
 import { TramitesOServiciosScreen } from '../screens/TramitesOServiciosScreen';
+import { FormProviderMetadatos } from '../../TramitesServicios/context/MetadatosContext';
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const TramitesServiciosRouter = (): ReactElement => {
   return (
-    <UserProvider>
-      <Routes>
-        <Route
-          path="tramites_o_servicios/*"
-          element={<TramitesOServiciosScreen />}
-        />
-        <Route path="/*" element={<Page404 />} />
-      </Routes>
-    </UserProvider>
+    <FormProviderMetadatos>
+      <UserProvider>
+        <Routes>
+          <Route
+            path="tramites_o_servicios/*"
+            element={<TramitesOServiciosScreen />}
+          />
+          <Route path="/*" element={<Page404 />} />
+        </Routes>
+      </UserProvider>
+    </FormProviderMetadatos>
   );
 };
