d98bab5aeb76f012053a183d49a8308bc8e6451d
HectorStiven <vigoyahecto90@gmail.com>
2024-03-26 04:35:26 -0500

    modal de metadatos ajustado a un 95 %

--- src/commons/gestorDocumental/TramitesOServicios/router/TramitesServiciosRouter.tsx
+++ src/commons/gestorDocumental/TramitesOServicios/router/TramitesServiciosRouter.tsx
@@ -9,14 +9,23 @@ import { FormProviderMetadatos } from '../../TramitesServicios/context/Metadatos
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const TramitesServiciosRouter = (): ReactElement => {
   return (
+    <FormProviderMetadatos>
+
     <UserProvider>
       <Routes>
         <Route
           path="tramites_o_servicios/*"
           element={<TramitesOServiciosScreen />}
         />
+        
+           <Route
+          path="respuesta_requerimiento_opa/*"
+          element={<ResReqOpaRouter/>}
+        />
         <Route path="/*" element={<Page404 />} />
       </Routes>
     </UserProvider>
+    </FormProviderMetadatos>
+
   );
 };
