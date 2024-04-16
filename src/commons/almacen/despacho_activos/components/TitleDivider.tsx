beda3f74901728866e6aab73833ac29f8dcf1374
rickbroken <daironquebrada@gmail.com>
2024-04-16 07:33:05 -0500

    feature / se agregan componentes para reutilizarlos en el modulo

---
+++ src/commons/almacen/despacho_activos/components/TitleDivider.tsx
@@ -0,0 +1,20 @@
+import { Chip, Divider, Grid } from '@mui/material';
+import React, { FC } from 'react';
+
+interface props {
+  title: string;
+}
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const TitleDivider: FC<props> = ({ title }) => {
+  return (
+    <Grid item xs={12}>
+      <Divider orientation="horizontal" variant="fullWidth" style={{ marginBlock: 'auto', width: '100%' }}>
+        <Chip label={title} size="small" />
+      </Divider>
+    </Grid>
+  );
+}
+
+// eslint-disable-next-line no-restricted-syntax
+export default TitleDivider;
