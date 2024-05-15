d43e219d8a15b8570a18a547869d99733e9c6cc3
rickbroken <daironquebrada@gmail.com>
2024-05-14 21:39:55 -0500

    feature / se agregan actualizaciones en reportes, se agrega paginacion en tabla de modal de entrada de bienes

--- src/components/BusquedaArticulos.tsx
+++ src/components/BusquedaArticulos.tsx
@@ -14,13 +14,21 @@ import {
 import { Column } from 'primereact/column';
 import { DataTable } from 'primereact/datatable';
 import { type Dispatch, type SetStateAction, useEffect, useState } from 'react';
-import { obtener_bienes, obtener_entradas_filtros } from '../commons/almacen/entradaDeAlmacen/thunks/Entradas';
+import { obtener_bienes, obtener_bienes_por_pagina, obtener_entradas_filtros } from '../commons/almacen/entradaDeAlmacen/thunks/Entradas';
 import { useAppDispatch } from '../hooks';
 import { Title } from './Title';
 import SearchIcon from '@mui/icons-material/Search';
 import { download_xls_dos } from '../documentos-descargar/XLS_descargar';
 import { download_pdf_dos } from '../documentos-descargar/PDF_descargar';
 import CleaningServicesIcon from '@mui/icons-material/CleaningServices';
+import { DataGrid, GridColDef, GridRowId } from '@mui/x-data-grid';
+import { v4 as uuidv4 } from 'uuid';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+
+interface custom_column extends GridColDef {
+  renderCell?: (params: { row: any[] }) => React.ReactNode;
+}
 
 interface IProps {
   is_modal_active: boolean;
@@ -32,8 +40,7 @@ export const BusquedaArticulos: React.FC<IProps> = (props: IProps) => {
   const dispatch = useAppDispatch();
   const [codigo, set_codigo] = useState<string>('');
   const [nombre, set_nombre] = useState<string>('');
-  const [grid_busqueda, set_grid_busqueda] = useState<any[]>([]);
-  const [grid_busqueda_before, set_grid_busqueda_before] = useState<any[]>([]);
+  const [bienes, set_bienes] = useState<any>(null);
   const [seleccion_articulo, set_seleccion_articulo] = useState<Record<string, any> | null>(null);
 
   const on_change_codigo: any = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -46,18 +53,18 @@ export const BusquedaArticulos: React.FC<IProps> = (props: IProps) => {
 
   const accionar_busqueda: any = () => {
     /*if (nombre === '' && codigo === '') {
-      set_grid_busqueda(grid_busqueda_before);
+      set_bienes(bienes_before);
       return;
     }
-    const data_filter = grid_busqueda_before.filter(
+    const data_filter = bienes_before.filter(
       (gv) =>
         Boolean(gv.nombre.includes(nombre)) &&
         gv.codigo_bien.toString().includes(codigo)
     );*/
-    dispatch(obtener_entradas_filtros(codigo ?? '',nombre ?? '')).then(
+    dispatch(obtener_entradas_filtros(codigo ?? '', nombre ?? '')).then(
       (response: { success: boolean; detail: string; data: any[] }) => {
         if (Object.keys(response).length !== 0) {
-          set_grid_busqueda(response.data);
+          set_bienes(response.data);
         }
       }
     );
@@ -66,49 +73,121 @@ export const BusquedaArticulos: React.FC<IProps> = (props: IProps) => {
   const limpiar_form = () => {
     set_codigo('');
     set_nombre('');
-    set_grid_busqueda(grid_busqueda_before);
   }
 
   const selected_product_grid: any = () => {
     props.articulo(seleccion_articulo);
     props.set_is_modal_active(false);
+    console.log(bienes);
   };
 
   useEffect(() => {
     dispatch(obtener_bienes()).then(
-      (response: { success: boolean; detail: string; data: any[] }) => {
+      (response: any) => {
         if (response.success) {
           const articulos = response.data.filter(
             (e: { nivel_jerarquico: number }) => e.nivel_jerarquico === 5
           );
-          set_grid_busqueda(articulos);
-          set_grid_busqueda_before([...articulos]);
+          set_bienes(() => {
+            return {
+              pagination: response.pagination,
+              articulos: articulos
+            };
+          });
         }
       }
     );
   }, []);
-  const columnsss = [
+  const columnsss: custom_column[] = [
     {
       field: 'id_bien',
-      header: 'Id',
-      style: { width: '25%' },
+      headerName: 'Id',
+      minWidth: 80,
+      maxWidth: 100
     },
     {
       field: 'codigo_bien',
-      header: 'Código',
-      style: { width: '25%' },
+      headerName: 'Código',
+      minWidth: 160,
     },
     {
       field: 'nombre',
-      header: 'Nombre',
-      style: { width: '25%' },
+      headerName: 'Nombre',
+      minWidth: 350,
     },
     {
-      field: 'cod_tipo_bien',
-      header: 'Tipo bien',
-      style: { width: '25%' },
+      field: 'tipo_bien',
+      headerName: 'Tipo bien',
+      minWidth: 100
     },
   ];
+
+  const handle_seleccionar_filas = (selectionModel: GridRowId[]) => {
+    // Filtrar objetos basados en los IDs seleccionados
+    const filas_filtradas = selectionModel.map(id_bien => {
+      // Encontrar el objeto con el ID correspondiente
+      return bienes?.articulos.find((bien : any) => bien.id_bien === id_bien);
+    });
+
+    // Filtrar objetos nulos (por si no se encuentra coincidencia)
+    const filas_filtradas_validas = filas_filtradas.filter(bien => bien !== undefined);
+
+    // Actualizar el estado con las filas seleccionadas
+    set_seleccion_articulo(filas_filtradas_validas[0]);
+  };
+
+  const pagina_siguiente = () => {
+    if (Number(bienes.pagination.pagina_actual) === Number(bienes.pagination.total_paginas)) {
+      return;
+    }
+
+    set_bienes(null);
+
+    dispatch(obtener_bienes_por_pagina(
+      (Number(bienes.pagination.pagina_actual) + 1).toString())
+    ).then(
+      (response: any) => {
+        if (response.success) {
+          const articulos = response.data.filter(
+            (e: { nivel_jerarquico: number }) => e.nivel_jerarquico === 5
+          );
+          set_bienes(() => {
+            return {
+              pagination: response.pagination,
+              articulos: articulos
+            };
+          });
+        }
+      }
+    );
+  }
+
+  const pagina_atras = () => {
+    if (Number(bienes.pagination.pagina_actual) === 1) {
+      return;
+    }
+    
+    set_bienes(null);
+
+    dispatch(obtener_bienes_por_pagina(
+      (Number(bienes.pagination.pagina_actual) - 1).toString())
+    ).then(
+      (response: any) => {
+        if (response.success) {
+          const articulos = response.data.filter(
+            (e: { nivel_jerarquico: number }) => e.nivel_jerarquico === 5
+          );
+          set_bienes(() => {
+            return {
+              pagination: response.pagination,
+              articulos: articulos
+            };
+          });
+        }
+      }
+    );
+  }
+
 
   return (
     // eslint-disable-next-line @typescript-eslint/no-misused-promises
@@ -140,7 +219,7 @@ export const BusquedaArticulos: React.FC<IProps> = (props: IProps) => {
                   onChange={on_change_codigo}
                 />
               </Grid>
-              
+
               <Grid item xs={12} lg={4}>
                 <TextField
                   label="Nombre"
@@ -198,53 +277,104 @@ export const BusquedaArticulos: React.FC<IProps> = (props: IProps) => {
                     }}
                   >
                     {download_xls_dos({
-                      nurseries: grid_busqueda,
+                      nurseries: bienes?.articulos,
                       columns: columnsss,
                     })}
                     {download_pdf_dos({
-                      nurseries: grid_busqueda,
+                      nurseries: bienes?.articulos,
                       columns: columnsss,
                       title: 'Resultados',
                     })}
                   </ButtonGroup>
-                  <div className="card">
-                    <DataTable
-                      value={grid_busqueda}
-                      sortField="nombre"
-                      stripedRows
-                      paginator
-                      rows={10}
-                      rowsPerPageOptions={[5, 10, 25, 50]}
-                      tableStyle={{ minWidth: '50rem' }}
-                      selectionMode="single"
-                      selection={seleccion_articulo}
-                      onSelectionChange={(e) => {
-                        set_seleccion_articulo(e.value as any);
-                      }}
-                      dataKey="id_bien"
+                  <DataGrid
+                    style={{ margin: '15px 0px' }}
+                    density="compact"
+                    loading={bienes === null}
+                    autoHeight
+                    rows={bienes?.articulos ?? []}
+                    columns={columnsss ?? []}
+                    pageSize={10}
+                    rowHeight={55}
+                    experimentalFeatures={{ newEditingApi: true }}
+                    onSelectionModelChange={handle_seleccionar_filas}
+                    getRowId={(res) => res?.id_bien === undefined ? uuidv4() : res.id_bien}
+                    hideFooterPagination
+                  />
+                  <Grid item xs={12} container sx={{
+                    display: 'flex',
+                    justifyContent: 'end',
+                    alignItems: 'center',
+                    mt: '20px',
+                  }}>
+                    <Grid item sx={{
+                      cursor: 'pointer',
+                      background: '#F5F5F5',
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      width: '40px',
+                      height: '40px',
+                      borderRadius: '50%',
+                      boxShadow: '0px 3px 6px #042F4A26',
+                      // quitamos el hover si es la primera pagina
+                      ...(Number(bienes?.pagination.pagina_actual) === 1 && {
+                        pointerEvents: 'none',
+                        background: '#f2f2f2',
+                      }),
+                      '&:hover': {
+                        background: '#E0E0E0',
+                        transition: 'all 0.3s ease',
+                      },
+                      '&:active': {
+                        background: '#BDBDBD',
+                        transition: 'all 0.3s ease',
+                      }
+                    }}
+                      onClick={() => pagina_atras()}
                     >
-                      <Column
-                        field="id_bien"
-                        header="Id"
-                        style={{ width: '25%' }}
-                      ></Column>
-                      <Column
-                        field="codigo_bien"
-                        header="Código"
-                        style={{ width: '25%' }}
-                      ></Column>
-                      <Column
-                        field="nombre"
-                        header="Nombre"
-                        style={{ width: '25%' }}
-                      ></Column>
-                      <Column
-                        field="tipo_bien"
-                        header="Tipo bien"
-                        style={{ width: '25%' }}
-                      ></Column>
-                    </DataTable>
-                  </div>
+                      <ChevronLeftIcon />
+                    </Grid>
+
+                    <Grid item sx={{
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      color: '#042F4A',
+                      mx: '20px',
+                    }}>
+                      {bienes?.pagination.pagina_actual ?? '...'} de {bienes?.pagination.total_paginas ?? '...'}
+                    </Grid>
+
+                    <Grid item sx={{
+                      cursor: 'pointer',
+                      background: '#F5F5F5',
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      width: '40px',
+                      height: '40px',
+                      borderRadius: '50%',
+                      boxShadow: '0px 3px 6px #042F4A26',
+                      // quitamos el hover si es la ultima pagina
+                      ...(Number(bienes?.pagination.pagina_actual) === Number(bienes?.pagination.total_paginas) && {
+                        pointerEvents: 'none',
+                        background: '#F5F5F5',
+                      }),
+                      '&:hover': {
+                        background: '#E0E0E0',
+                        transition: 'all 0.3s ease',
+                      },
+                      '&:active': {
+                        background: '#BDBDBD',
+                        transition: 'all 0.3s ease',
+                      }
+                    }}
+                      onClick={() => pagina_siguiente()}
+                    >
+                      <NavigateNextIcon />
+                    </Grid>
+
+                  </Grid>
                 </Box>
               </Grid>
             </Grid>
