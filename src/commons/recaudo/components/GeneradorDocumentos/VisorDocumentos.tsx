0de0254720e2ef59d7bc17dd3e61c6144654d5bc
julemole <julian.moreno.lesmes@unillanos.edu.co>
2024-05-23 18:30:27 -0500

    Modal confirmación firma

--- src/commons/recaudo/components/GeneradorDocumentos/VisorDocumentos.tsx
+++ src/commons/recaudo/components/GeneradorDocumentos/VisorDocumentos.tsx
@@ -17,19 +17,24 @@ import {
   Typography,
 } from '@mui/material';
 import CloudDownloadIcon from '@mui/icons-material/CloudDownload';
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import CreateIcon from '@mui/icons-material/Create';
 import DocViewer, { DocViewerRenderers } from '@cyntler/react-doc-viewer';
+import { ModalConfirmacionMail } from './ModalConfirmacionMail';
+import { useState } from 'react';
 
 export const VisorDocumentos: React.FC<any> = ({file}: {file: any}) => {
+  const [open, setOpen] = useState(false);
+
   return (
     <Grid item xs={12} sm={12}>
       {(file) && (
         <>
-          <Grid sx={{display: 'flex', m: 'auto', width: '200px', mb: '1.5rem'}}>
+          <Grid sx={{display: 'flex', justifyContent: 'center', gap: '1rem', mb: '1.5rem'}}>
             <Button
               fullWidth
               variant="contained"
               color="primary"
+              sx={{width: '200px'}}
               href={file}
               download
               endIcon={<CloudDownloadIcon />}
@@ -38,9 +43,11 @@ export const VisorDocumentos: React.FC<any> = ({file}: {file: any}) => {
             </Button>
             <Button
               fullWidth
+              sx={{width: '250px'}}
               variant="contained"
               color="primary"
-              endIcon={<CloudDownloadIcon />}
+              endIcon={<CreateIcon />}
+              onClick={() => setOpen(true)}
             >
               Firmar Documento
             </Button>
@@ -49,6 +56,10 @@ export const VisorDocumentos: React.FC<any> = ({file}: {file: any}) => {
             pluginRenderers={DocViewerRenderers}
             documents={[{ uri: file, fileType: 'docx' }]}
             style={{height: 800, width: '70%', display: 'flex', margin: 'auto'}}
+          />
+          <ModalConfirmacionMail
+            open={open}
+            setOpen={setOpen}
           />
         </>
       )}
